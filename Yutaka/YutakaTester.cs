55701164514b7cfba963490ee57bfbe40061683b
blizmanyu <blizman2017@gmail.com>
2017-08-29 19:03:29 -0700

    2017 0829 1903

--- Yutaka/YutakaTester.cs
+++ Yutaka/YutakaTester.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Yutaka.IO;
+using FileUtil = Yutaka.FileUtil;
 
 namespace Yutaka
 {
@@ -6,6 +9,11 @@ namespace Yutaka
 	{
 		static void Main(string[] args)
 		{
+			var paths = new string[] { @"\\DC-RCW\Public\images\Coins\134637b.jpg", @"\\DC-RCW\Public\images\Coins\134671b.jpg", @"\\DC-RCW\Public\images\Coins\134637bc.jpg" };
+			var fileInfos = new FileInfo[] { new FileInfo(paths[0]), new FileInfo(paths[1]), new FileInfo(paths[2]) };
+
+			Console.Write("\n\n======= IsSameDate(FileInfo fi1, FileInfo fi2) Test =======");
+			//Console.Write("\n{0}: {1}", FileUtil.IsSameDate(v));
 		}
 	}
 }
