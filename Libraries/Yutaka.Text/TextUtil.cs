24543e9927457c873094e55925e73157e1ec0928
blizmanyu <blizman2017@gmail.com>
2017-10-24 14:31:04 -0700

    2017 1024 1431

--- Libraries/Yutaka.Text/TextUtil.cs
+++ Libraries/Yutaka.Text/TextUtil.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yutaka.Utils;
 
 namespace Yutaka.Text
 {
-	public class TextUtil
+	public static class TextUtil
 	{
+		public static string ConvertIpToBase36(string ip, bool lowerCase=true)
+		{
+			if (ip == null)
+				throw new ArgumentNullException(ip);
+			if (ip.Length < 7 || ip.Length > 16)
+				throw new ArgumentOutOfRangeException(ip, ip, "Enter a value between 0.0.0.0 and 255.255.255.255");
+
+			var sb = new StringBuilder();
+			ip.Split('.').ToList().ForEach(u => sb.Append(u.ToString().PadLeft(3, '0')));
+			sb.Replace(".","");
+
+			Int64 result;
+			if (Int64.TryParse(sb.ToString(), out result))
+				return Base36.Encode(result, lowerCase);
+
+			return null;
+		}
+
+		public static Int64 ConvertIpToInt64(string ip)
+		{
+			if (ip == null)
+				throw new ArgumentNullException(ip);
+			if (ip.Length < 7 || ip.Length > 16)
+				throw new ArgumentOutOfRangeException(ip, ip, "Enter a value between 0.0.0.0 and 255.255.255.255");
+
+			var sb = new StringBuilder();
+			ip.Split('.').ToList().ForEach(u => sb.Append(u.ToString().PadLeft(3, '0')));
+			sb.Replace(".", "");
+
+			Int64 result;
+			if (Int64.TryParse(sb.ToString(), out result))
+				return result;
+
+			return -1;
+		}
 	}
 }
