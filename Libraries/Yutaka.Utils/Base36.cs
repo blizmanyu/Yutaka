d46dd45f8c703580cfff7b06e05e10ade7e61fbf
blizmanyu <blizman2017@gmail.com>
2017-10-23 18:20:36 -0700

    2017 1023 1820

--- Libraries/Yutaka.Utils/Base36.cs
+++ Libraries/Yutaka.Utils/Base36.cs
@@ -6,7 +6,7 @@ namespace Yutaka.Utils
 {
 	public static class Base36
 	{
-		const string CharList = "0123456789abcdefghijklmnopqrstuvwxyz";
+		const string CharListLower = "0123456789abcdefghijklmnopqrstuvwxyz";
 		const string CharListUpper = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 		public static string Encode(long input, bool lowerCase = true)
@@ -14,12 +14,13 @@ namespace Yutaka.Utils
 			if (input < 0)
 				throw new ArgumentOutOfRangeException("input", input, "input cannot be negative");
 
-			char[] clistarr;
+			string CharList;
 			if (lowerCase)
-				clistarr = CharList.ToCharArray();
+				CharList = CharListLower;
 			else
-				clistarr = CharListUpper.ToCharArray();
+				CharList = CharListUpper;
 
+			char[] clistarr = CharList.ToCharArray();
 			var result = new Stack<char>();
 			while (input != 0) {
 				result.Push(clistarr[input % 36]);
@@ -30,14 +31,17 @@ namespace Yutaka.Utils
 
 		public static Int64 Decode(string input, bool lowerCase = true)
 		{
+			string CharList;
+			if (lowerCase)
+				CharList = CharListLower;
+			else
+				CharList = CharListUpper;
+
 			var reversed = input.Reverse();
 			long result = 0;
 			var pos = 0;
 			foreach (char c in reversed) {
-				if (lowerCase)
-					result += CharList.IndexOf(c) * (long) Math.Pow(36, pos);
-				else
-					result += CharListUpper.IndexOf(c) * (long) Math.Pow(36, pos);
+				result += CharList.IndexOf(c) * (long) Math.Pow(36, pos);
 				pos++;
 			}
 			return result;
