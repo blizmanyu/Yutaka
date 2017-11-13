72ef0bdd698e47c8e77d93402fbbe6d50ab27d77
blizmanyu <blizman2017@gmail.com>
2017-11-13 15:33:54 -0800

    2017 1113 1533

---
+++ Libraries/Yutaka.Net/MailUtil.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Yutaka.Net
+{
+	public static class MailUtil
+	{
+		#region Methods
+		public static Result Send(MailMessage message, string smtpHost, int smtpPort, string username, string password, SmtpDeliveryMethod smtpDeliveryMethod = SmtpDeliveryMethod.Network, bool enableSsl = true, bool useDefaultCredentials = false)
+		{
+			var result = new Result() {
+				Success = false,
+				Message = "",
+				Exception = ""
+			};
+
+			using (var client = new SmtpClient(smtpHost, smtpPort)) {
+				client.Credentials = new NetworkCredential(username, password);
+				client.DeliveryMethod = smtpDeliveryMethod;
+				client.EnableSsl = enableSsl;
+				client.UseDefaultCredentials = false;
+
+				try {
+					client.Send(message);
+					result.Success = true;
+				}
+
+				catch (Exception ex) {
+					result.Message = ex.Message;
+					result.Exception = ex.ToString();
+				}
+			}
+
+			return result;
+		}
+
+		public static Result Send(SmtpClient client, MailMessage message)
+		{
+			var result = new Result() {
+				Success = false,
+				Message = "",
+				Exception = ""
+			};
+
+			try {
+				client.Send(message);
+				result.Success = true;
+			}
+
+			catch (Exception ex) {
+				result.Message = ex.Message;
+				result.Exception = ex.ToString();
+			}
+
+			return result;
+		}
+
+		public static Result Send(SmtpClient client, string from, string to, string subject, string body, AttachmentCollection attachments=null, MailAddressCollection bcc = null, MailAddressCollection cc = null, bool isBodyHtml = true)
+		{
+			var result = new Result() {
+				Success = false,
+				Message = "",
+				Exception = ""
+			};
+
+			using (var message = new MailMessage(from, to, subject, body)) {
+				int i;
+
+				if (attachments != null) {
+					for (i = 0; i < attachments.Count; i++)
+						message.Attachments.Add(attachments[i]);
+				}
+
+				if (bcc != null) {
+					for (i = 0; i < bcc.Count; i++)
+						message.Bcc.Add(bcc[i]);
+				}
+
+				if (cc != null) {
+					for (i = 0; i < cc.Count; i++)
+						message.CC.Add(cc[i]);
+				}
+
+				message.IsBodyHtml = isBodyHtml;
+
+				try {
+					client.Send(message);
+					result.Success = true;
+				}
+
+				catch (Exception ex) {
+					result.Message = ex.Message;
+					result.Exception = ex.ToString();
+				}
+			}
+
+			return result;
+		}
+
+		public static Result Send(string smtpHost, int smtpPort, string username, string password, string from, string to, string subject, string body, SmtpDeliveryMethod smtpDeliveryMethod = SmtpDeliveryMethod.Network, bool enableSsl = true, bool useDefaultCredentials = false, AttachmentCollection attachments = null, MailAddressCollection bcc = null, MailAddressCollection cc = null, bool isBodyHtml = true)
+		{
+			var result = new Result() {
+				Success = false,
+				Message = "",
+				Exception = ""
+			};
+
+			using (var message = new MailMessage(from, to, subject, body)) {
+				int i;
+
+				if (attachments != null) {
+					for (i = 0; i < attachments.Count; i++)
+						message.Attachments.Add(attachments[i]);
+				}
+
+				if (bcc != null) {
+					for (i = 0; i < bcc.Count; i++)
+						message.Bcc.Add(bcc[i]);
+				}
+
+				if (cc != null) {
+					for (i = 0; i < cc.Count; i++)
+						message.CC.Add(cc[i]);
+				}
+
+				message.IsBodyHtml = isBodyHtml;
+
+				using (var client = new SmtpClient(smtpHost, smtpPort)) {
+					client.Credentials = new NetworkCredential(username, password);
+					client.DeliveryMethod = smtpDeliveryMethod;
+					client.EnableSsl = enableSsl;
+					client.UseDefaultCredentials = false;
+
+					try {
+						client.Send(message);
+						result.Success = true;
+					}
+
+					catch (Exception ex) {
+						result.Message = ex.Message;
+						result.Exception = ex.ToString();
+					}
+				}
+			}
+
+			return result;
+		}
+		#endregion
+
+		#region Struct
+		public struct Result
+		{
+			public bool Success;
+			public string Message;
+			public string Exception;
+		}
+		#endregion
+	}
+}
