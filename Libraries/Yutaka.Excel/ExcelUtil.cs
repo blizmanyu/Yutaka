4f814c2092b4ef5b8bd7b0fc5789e02fd5927cc2
blizmanyu <blizman2017@gmail.com>
2018-02-20 17:44:59 -0800

    Read(string path, bool hasHeader=true)

--- Libraries/Yutaka.Excel/ExcelUtil.cs
+++ Libraries/Yutaka.Excel/ExcelUtil.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OfficeOpenXml;
 
 namespace Yutaka.Excel
 {
-    public class ExcelUtil
+    public static class ExcelUtil
     {
-    }
+		public static void Read(Stream stream, bool hasHeader=true)
+		{
+
+		}
+
+		public static void Read(string path, bool hasHeader=true)
+		{
+			if (String.IsNullOrEmpty(path))
+				throw new ArgumentNullException("path", @"<path> is required.");
+
+			using (var ePackage = new ExcelPackage()) {
+				using (var stream = File.OpenRead(path)) {
+					ePackage.Load(stream);
+				}
+
+				var worksheet = ePackage.Workbook.Worksheets.FirstOrDefault();
+
+				if (worksheet == null)
+					throw new Exception("No worksheet found");
+				if (worksheet.Dimension == null)
+					throw new Exception("Empty worksheet");
+
+				//foreach (var firstRowCell in worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column]) {
+				//	tbl.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
+				//}
+				//var startRow = hasHeader ? 2 : 1;
+				//for (int rowNum = startRow; rowNum <= worksheet.Dimension.End.Row; rowNum++) {
+				//	var wsRow = worksheet.Cells[rowNum, 1, rowNum, worksheet.Dimension.End.Column];
+				//	DataRow row = tbl.Rows.Add();
+				//	foreach (var cell in wsRow) {
+				//		row[cell.Start.Column - 1] = cell.Text;
+				//	}
+				//}
+				//return tbl;
+			}
+		}
+	}
 }
