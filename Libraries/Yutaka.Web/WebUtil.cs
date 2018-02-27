4378504fcd507dedef65f83fc1260aac94eaad38
blizmanyu <blizman2017@gmail.com>
2018-02-27 14:06:03 -0800

    Removed Yutaka.Web

--- Libraries/Yutaka.Web/WebUtil.cs
+++ Libraries/Yutaka.Web/WebUtil.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Yutaka.Web
 {
-    public class WebUtil
-    {
-    }
+	public static class WebUtil
+	{
+		#region
+		public static 
+		#endregion
+	}
 }
