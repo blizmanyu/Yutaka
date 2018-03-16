3e21808c5a6e452171f86102ed7ccb0aa44ec553
blizmanyu <blizman2017@gmail.com>
2018-03-16 14:24:00 -0700

    Created WebUtil.cs

---
+++ Libraries/Yutaka.Web/WebUtil.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Yutaka.Utils;
+
+namespace Yutaka.Web
+{
+    public class WebUtil : Controller
+    {
+		public WebUtil() { }
+
+		#region Methods
+		public void SetSessionVariables()
+		{
+			var uniqueId = Base36.UniqueID();
+			var url = Request.Url;
+			var referer = Request.UrlReferrer;
+
+			if (Session["SessionId"] == null)
+				Session["SessionId"] = uniqueId;
+			if (Session["Orig_Referer"] == null)
+				Session["Orig_Referer"] = referer == null ? "" : referer.AbsoluteUri;
+
+			Session["Url"] = url == null ? "" : url.AbsoluteUri;
+			Session["Referer"] = referer == null ? "" : referer.AbsoluteUri;
+		}
+		#endregion
+	}
+}
