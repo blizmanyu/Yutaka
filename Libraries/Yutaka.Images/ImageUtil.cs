f3a5a4953821741201b0936b89f4e5e209d52e36
blizmanyu <blizman2017@gmail.com>
2018-05-03 12:11:52 -0700

    New UrlExistsAndValid(string url)

--- Libraries/Yutaka.Images/ImageUtil.cs
+++ Libraries/Yutaka.Images/ImageUtil.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
+using System.Net;
 
 namespace Yutaka.Images
 {
 	public static class ImageUtil
 	{
+		private static HttpWebRequest request;
+
 		public static bool FileExistsAndValid(string filepath)
 		{
 			if (String.IsNullOrEmpty(filepath) || !File.Exists(filepath))
@@ -14,6 +17,32 @@ namespace Yutaka.Images
 			try {
 				using (var bmp = new Bitmap(filepath)) { }
 				return true;
+			}
+
+			catch (Exception) {
+				return false;
+			}
+		}
+
+		public static bool UrlExistsAndValid(string url)
+		{
+			if (String.IsNullOrEmpty(url))
+				return false;
+
+			request = (HttpWebRequest) WebRequest.Create(url);
+			request.KeepAlive = true;
+			request.Method = "HEAD";
+			request.Proxy = null;
+
+			try {
+				using (var response = (HttpWebResponse) request.GetResponse()) {
+					var len = response.ContentLength;
+
+					if (len > 1024)
+						return true;
+					else
+						return false;
+				}
 			}
 
 			catch (Exception) {
