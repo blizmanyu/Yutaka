69a3c2679c815fd85e07102c0687c4b8f4ec6b45
blizmanyu <blizman2017@gmail.com>
2018-12-28 16:47:02 -0800

    Removed GmailApiUtil

--- Programs/GmailApiUtil/GmailApiUtil.cs
+++ Programs/GmailApiUtil/GmailApiUtil.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GmailApiUtil
 {
     public class GmailApiUtil
     {
-    }
+		public string[] Scopes { get; set; }
+		public string ApplicationName { get; set; }
+
+		public GmailApiUtil() { }
+
+		public GmailService GetGmailService(string userEmail)
+		{
+
+		}
+	}
 }
