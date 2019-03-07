c542956f1da287979ded4e22695d083549d3d124
blizmanyu <blizman2017@gmail.com>
2019-03-06 22:34:36 -0800

    2019 0306 2234

--- Tests/Yutaka.Tests/Program.cs
+++ Tests/Yutaka.Tests/Program.cs
@@ -56,19 +56,11 @@ namespace Yutaka.Tests
 			VideoUtil _videoUtil;
 
 			var tests = new string[] {
-				@"G:\Projects\FileCopier2\Downloads\Nicole Black Dee Williams - LegalPorno 201903 Good afternoon miss Black #2 - Balls Deep Anal, Gapes, DAP, ATOGM, Cumswapping with Swallow GIO941 dp 4k 2160p.mp4",
-				//@"F:\Videos\Jful\Alex Grey - Anal Savages Scene 4 JulesJordan 201605 Glamour Model Takes It Up The Ass anal 4k 2160p.mp4",
-				//@"F:\Videos\Jful\Alex Grey - Holed 201609 Babysitter Gets Raw Anal 4k 2160p.mp4",
-				//@"F:\Videos\Jful\Alex Grey - Slut Puppies 11 Scene Teen Ass Is Open For Business anal 4k 2160p.mp4",
-				//@"F:\Videos\Jful\Alex Grey - Tushy 201602 Beautiful Petite Blonde Orgasm with 1st First Anal 1080p.mp4",
-				////@"F:\Videos\Jful\Alex Grey - Tushy 201605 Bratty Rich Girl Gets More Than She Bargained For anal 1080p.mp4",
-				//@"F:\Videos\Jful\Alex Grey - Young Tight Sluts 2 – Scene 3 anal finger 720p.mp4",
-				//@"F:\Videos\Jful\Alex Grey - HardX 201609 Alex Grey Does Anal 1080p.mp4",
-				//@"F:\Videos\Jful\Alex Grey - MrStrokesXXX 201607 Alex Grey’s 1st DP & 1st first Interracial anal 1080p.mp4",
-				//@"F:\Videos\Jful\Alex Grey, Karla Kush - Tushy 201606 Gorgeous Young Girlfriends Try Anal Together 1080p.part1.mp4",
+				//@"G:\Projects\FileCopier2\Downloads\Nicole Black Dee Williams - LegalPorno 201903 Good afternoon miss Black #2 - Balls Deep Anal, Gapes, DAP, ATOGM, Cumswapping with Swallow GIO941 dp 4k 2160p.mp4",
 				//@"F:\Videos\Jful\Katrina Jade GloryHole 201602 1080p.mp4",
 				//@"F:\Videos\Jful\Princess Leia - Double Let Down [JulesJordan] (2015 anal dp 720p.mp4",
-				//@"F:\Videos\Jful\Sheena Shaw - 201608 The Milk Enema anal 1080p.mp4",
+				@"F:\Videos\Jful\Sheena Shaw - 201608 The Milk Enema anal 1080p.mp4",
+				//@"C:\Downloads\Videos\Silvia Dellai - LegalPorno 201903 anal 3on1, DP, DVP & prone bone pounding SZ2085 swallow 4k 2160p.mp4",
 			};
 
 			for (int i = 0; i < tests.Length; i++) {
