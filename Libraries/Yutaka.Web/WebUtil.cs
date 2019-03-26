2625fe84441212444a079e9dd62d2eededafb923
blizmanyu <blizman2017@gmail.com>
2019-03-25 17:38:38 -0700

    New method string GetCookieValue(string fieldName)

--- Libraries/Yutaka.Web/WebUtil.cs
+++ Libraries/Yutaka.Web/WebUtil.cs
@@ -16,28 +16,12 @@ namespace Yutaka.Web
 		// Updated Aug 1, 2014 // Retrieved Apr 5, 2018 //
 		private Regex MobileCheck = new Regex(@"(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled);
 		private Regex MobileVersionCheck = new Regex(@"1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled);
+		protected MailUtil _mailUtil;
+		public string CookieName;
 
-		public WebUtil() { }
-
-		public string EncodeIp(string ipAddress)
-		{
-			if (String.IsNullOrWhiteSpace(ipAddress))
-				throw new Exception(String.Format("<ipAddress> is required.{0}Exception thrown in WebUtil.EncodeIp(string ipAddress){0}", Environment.NewLine));
-			if (ipAddress.Length < 7 || 16 < ipAddress.Length)
-				throw new Exception(String.Format("<ipAddress> must be between 0.0.0.0 and 255.255.255.255.{0}Exception thrown in WebUtil.EncodeIp(string ipAddress){0}", Environment.NewLine));
-
-			try {
-				var sb = new StringBuilder();
-				ipAddress.Split('.').ToList().ForEach(u => sb.Append(u.ToString().PadLeft(3, '0')));
-				return Base36.Encode(Int64.Parse(sb.ToString()));
-			}
-
-			catch (Exception ex) {
-				if (ex.InnerException == null)
-					throw new Exception(String.Format("{0}{2}Exception thrown in WebUtil.EncodeIp(string ipAddress='{3}'){2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, ipAddress));
-
-				throw new Exception(String.Format("{0}{2}Exception thrown in INNER EXCEPTION of WebUtil.EncodeIp(string ipAddress='{3}'){2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, ipAddress));
-			}
+		public WebUtil() {
+			_mailUtil = new MailUtil();
+			CookieName = "RCW2019.0325";
 		}
 
 		public string DecodeIp(string str)
@@ -60,6 +44,98 @@ namespace Yutaka.Web
 					throw new Exception(String.Format("{0}{2}Exception thrown in WebUtil.DecodeIp(string str='{3}'){2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, str));
 
 				throw new Exception(String.Format("{0}{2}Exception thrown in INNER EXCEPTION of WebUtil.DecodeIp(string str='{3}'){2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, str));
+			}
+		}
+
+		public string EncodeIp(string ipAddress)
+		{
+			if (String.IsNullOrWhiteSpace(ipAddress))
+				throw new Exception(String.Format("<ipAddress> is required.{0}Exception thrown in WebUtil.EncodeIp(string ipAddress){0}", Environment.NewLine));
+			if (ipAddress.Length < 7 || 16 < ipAddress.Length)
+				throw new Exception(String.Format("<ipAddress> must be between 0.0.0.0 and 255.255.255.255.{0}Exception thrown in WebUtil.EncodeIp(string ipAddress){0}", Environment.NewLine));
+
+			try {
+				var sb = new StringBuilder();
+				ipAddress.Split('.').ToList().ForEach(u => sb.Append(u.ToString().PadLeft(3, '0')));
+				return Base36.Encode(Int64.Parse(sb.ToString()));
+			}
+
+			catch (Exception ex) {
+				if (ex.InnerException == null)
+					throw new Exception(String.Format("{0}{2}Exception thrown in WebUtil.EncodeIp(string ipAddress='{3}'){2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, ipAddress));
+
+				throw new Exception(String.Format("{0}{2}Exception thrown in INNER EXCEPTION of WebUtil.EncodeIp(string ipAddress='{3}'){2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, ipAddress));
+			}
+		}
+
+		public string GetCookieValue(string fieldName)
+		{
+			if (String.IsNullOrWhiteSpace(fieldName))
+				throw new Exception(String.Format("<fieldName> is required.{0}Exception thrown in WebUtil.GetCookieValue(string name, string fieldName){0}", Environment.NewLine));
+
+			try {
+				var Request = HttpContext.Current.Request;
+				var cookie = Request.Cookies[CookieName];
+				var cookieRCW = Request.Cookies["RCW"];
+				var cookieRCW370 = Request.Cookies["RCW370"];
+				fieldName = fieldName.ToUpper();
+
+				switch (fieldName) {
+					#region case "ID"
+					case "ID":
+						if (cookie != null)
+							if (!String.IsNullOrWhiteSpace(cookie["id"]))
+								return cookie["id"];
+						if (cookieRCW != null)
+							if (!String.IsNullOrWhiteSpace(cookieRCW["cookieID"]))
+								return cookieRCW["cookieID"];
+						if (cookieRCW370 != null)
+							if (!String.IsNullOrWhiteSpace(cookieRCW370["Id"]))
+								return cookieRCW370["Id"];
+						break;
+					#endregion case "ID"
+					#region case "IP"
+					case "IP":
+						if (cookie != null)
+							if (!String.IsNullOrWhiteSpace(cookie["i"]))
+								return DecodeIp(cookie["i"]);
+						if (cookieRCW != null)
+							if (!String.IsNullOrWhiteSpace(cookieRCW["key2"]))
+								return cookieRCW["key2"];
+						break;
+					#endregion case "IP"
+					#region case "EMAIL"
+					case "EMAIL":
+						if (cookie != null)
+							if (!String.IsNullOrWhiteSpace(cookie["e"]))
+								return _mailUtil.DecodeEmail(cookie["e"]);
+						if (cookieRCW != null)
+							if (!String.IsNullOrWhiteSpace(cookieRCW["key1"]))
+								return cookieRCW["key1"];
+						if (cookieRCW370 != null)
+							if (!String.IsNullOrWhiteSpace(cookieRCW370["Em"]))
+								return cookieRCW370["Em"];
+						break;
+					#endregion case "EMAIL"
+					#region case "SOURCE"
+					case "SOURCE":
+						if (cookie != null)
+							if (!String.IsNullOrWhiteSpace(cookie["s"]))
+								return cookie["s"];
+						break;
+					#endregion case "SOURCE"
+					default:
+						break;
+				}
+
+				return "";
+			}
+
+			catch (Exception ex) {
+				if (ex.InnerException == null)
+					throw new Exception(String.Format("{0}{2}Exception thrown in WebUtil.GetCookieValue(string fieldName='{3}'){2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, fieldName));
+
+				throw new Exception(String.Format("{0}{2}Exception thrown in INNER EXCEPTION of WebUtil.GetCookieValue(string fieldName='{3}'){2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, fieldName));
 			}
 		}
 
@@ -175,15 +251,15 @@ namespace Yutaka.Web
 				// Email - can change if the //
 				if (String.IsNullOrWhiteSpace(cookie["e"]))
 					if (!String.IsNullOrWhiteSpace(email))
-						cookie["e"] = new MailUtil().EncodeEmail(email);
+						cookie["e"] = _mailUtil.EncodeEmail(email);
 
 				// Source - should never change unless null or empty //
 				if (String.IsNullOrWhiteSpace(cookie["s"])) {
 					if (String.IsNullOrWhiteSpace(source))
 						source = Request.UrlReferrer.AbsoluteUri ?? "";
 
-					if (source.Length > 2000)
-						source = source.Substring(0, 2000);
+					if (source.Length > 600)
+						source = source.Substring(0, 600);
 
 					cookie["s"] = source;
 				}
