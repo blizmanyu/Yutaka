c7e0d3ce0a6b2ff8529c7230464eb9aaf0b832f1
blizmanyu <blizman2017@gmail.com>
2019-05-13 03:09:35 -0700

    Fields plus Constructor

--- Libraries/Yutaka.IO/YuImage.cs
+++ Libraries/Yutaka.IO/YuImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,32 @@ namespace Yutaka.IO
 {
 	public class YuImage
 	{
+		public DateTime CreationTime;
+		public DateTime LastAccessTime;
+		public DateTime LastWriteTime;
+		public DateTime MinDateTime;
+		public long Length;
+		public string Extension;
+		public string FullName;
+		public string Name;
+		public string NewPath;
+		public string ParentDirectory;
+
+		public YuImage(string filename)
+		{
+			var fi = new FileInfo(filename);
+			CreationTime = fi.CreationTime;
+			LastAccessTime = fi.LastAccessTime;
+			LastWriteTime = fi.LastWriteTime;
+			//MinDateTime = fi.MinDateTime;
+			Length = fi.Length;
+			Extension = fi.Extension;
+			FullName = fi.FullName;
+			Name = fi.Name;
+			//NewPath = fi.NewPath;
+			//ParentDirectory = fi.ParentDirectory;
+
+			fi = null;
+		}
 	}
 }
