956284ed6e6c0cfc95f3b752326bf57ed8e8d3b8
blizmanyu <blizman2017@gmail.com>
2019-05-21 12:57:51 -0700

    New Class GmailSmtpClient

---
+++ Libraries/Yutaka.Net/GmailSmtpClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Yutaka.Net
+{
+	public class GmailSmtpClient : SmtpClient
+	{
+		public GmailSmtpClient(string username, string password)
+		{
+			if (String.IsNullOrWhiteSpace(username))
+				throw new Exception(String.Format("<username> is required. Exception thrown in GmailSmtpClient Constructor.{0}", Environment.NewLine));
+			if (String.IsNullOrWhiteSpace(password))
+				throw new Exception(String.Format("<password> is required. Exception thrown in GmailSmtpClient Constructor.{0}", Environment.NewLine));
+
+			Credentials = new NetworkCredential(username, password);
+			EnableSsl = true;
+			Host = "smtp.gmail.com";
+			Port = 587;
+			UseDefaultCredentials = false;
+		}
+	}
+}
