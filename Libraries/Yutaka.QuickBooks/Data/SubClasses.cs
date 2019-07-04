12c4ef0e356cb8ecbb06c189e402a31ebf66bb67
blizmanyu <blizman2017@gmail.com>
2019-07-03 18:04:00 -0700

    Added SubClasses

---
+++ Libraries/Yutaka.QuickBooks/Data/SubClasses.cs
@@ -0,0 +1,41 @@
+﻿namespace Yutaka.QuickBooks
+{
+	public class AccountRef
+	{
+		public string ListID;
+		public string FullName;
+	}
+
+	public class ClassRef
+	{
+		public string ListID;
+		public string FullName;
+	}
+
+	public class ParentRef
+	{
+		public string ListID;
+		public string FullName;
+	}
+
+	public class UnitOfMeasureSetRef
+	{
+		public string ListID;
+		public string FullName;
+	}
+
+	public class SalesTaxCodeRef
+	{
+		public string ListID;
+		public string FullName;
+	}
+
+	public class SalesOrPurchase
+	{
+		public AccountRef AccountRef;
+		public string Desc;
+		public decimal Price;
+		public decimal PricePercent;
+		public SalesOrPurchase() { AccountRef = new AccountRef(); }
+	}
+}
