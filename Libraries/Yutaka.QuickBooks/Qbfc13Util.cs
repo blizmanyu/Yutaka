6211808143fccb45c2faf7af6929d194593aae38
blizmanyu <blizman2017@gmail.com>
2019-07-08 16:28:34 -0700

    Made _logLevel public. New method public ItemNonInventoryRet XmlNodeToItemNonInventoryRet(XmlNode xml)

--- Libraries/Yutaka.QuickBooks/Qbfc13Util.cs
+++ Libraries/Yutaka.QuickBooks/Qbfc13Util.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.IO;
 using Interop.QBFC13;
 using Interop.QBXMLRP2Lib;
+using System.Xml;
 
 namespace Yutaka.QuickBooks
 {
@@ -14,11 +15,11 @@ namespace Yutaka.QuickBooks
 	{
 		#region Fields
 		const string TIMESTAMP = @"HH:mm:ss.fff";
-		private LogLevel _logLevel;
 		private QBSessionManager _sessionManager;
 		private bool _connectionOpen;
 		private bool _sessionBegun;
 		private string _appName;
+		public LogLevel _logLevel;
 		/// <summary>
 		/// Trace - very detailed logs, which may include high-volume information such as protocol payloads. This log level is typically only enabled during development. Ex: begin method X, end method X
 		/// Debug - debugging information, less detailed than trace, typically not enabled in production environment. Ex: executed query, user authenticated, session expired
@@ -51,6 +52,139 @@ namespace Yutaka.QuickBooks
 			_connectionOpen = false;
 			_sessionBegun = false;
 			_appName = appName;
+		}
+
+		public ItemNonInventoryRet XmlNodeToItemNonInventoryRet(XmlNode xml)
+		{
+			if (_logLevel <= LogLevel.Trace)
+				Console.Write("\n[{0}] Begin method XmlNodeToItemNonInventoryRet(XmlNode xml).", DateTime.Now.ToString(TIMESTAMP));
+			if (xml == null || !xml.HasChildNodes)
+				throw new Exception("<xml> is required. Exception thrown in Qbfc13Util.XmlNodeToItemNonInventoryRet(XmlNode xml).{0}{0}");
+
+			try {
+				XmlNode node, subNode;
+				var classRef = new ClassRef();
+				var parentRef = new ParentRef();
+				var unitOfMeasureSetRef = new UnitOfMeasureSetRef();
+				var salesTaxCodeRef = new SalesTaxCodeRef();
+				var salesOrPurchase = new SalesOrPurchase();
+				var salesAndPurchase = new SalesAndPurchase();
+
+				#region Get Subnodes
+				node = xml.SelectSingleNode("ClassRef");
+
+				if (node != null) {
+					classRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					classRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				node = xml.SelectSingleNode("ParentRef");
+
+				if (node != null) {
+					parentRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					parentRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				node = xml.SelectSingleNode("UnitOfMeasureSetRef");
+
+				if (node != null) {
+					unitOfMeasureSetRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					unitOfMeasureSetRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				node = xml.SelectSingleNode("SalesTaxCodeRef");
+
+				if (node != null) {
+					salesTaxCodeRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					salesTaxCodeRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				node = xml.SelectSingleNode("SalesOrPurchase");
+
+				if (node != null) {
+					salesOrPurchase.Desc = node.SelectSingleNode("Desc") == null ? "" : node.SelectSingleNode("Desc").InnerText;
+					salesOrPurchase.Price = node.SelectSingleNode("Price") == null ? 0 : decimal.Parse(node.SelectSingleNode("Price").InnerText);
+					salesOrPurchase.PricePercent = node.SelectSingleNode("PricePercent") == null ? 0 : decimal.Parse(node.SelectSingleNode("PricePercent").InnerText);
+					subNode = node.SelectSingleNode("AccountRef");
+
+					if (subNode != null) {
+						salesOrPurchase.AccountRef.ListID = subNode.SelectSingleNode("ListID") == null ? "" : subNode.SelectSingleNode("ListID").InnerText;
+						salesOrPurchase.AccountRef.FullName = subNode.SelectSingleNode("FullName") == null ? "" : subNode.SelectSingleNode("FullName").InnerText;
+					}
+				}
+
+				node = xml.SelectSingleNode("SalesAndPurchase");
+
+				if (node != null) {
+					salesAndPurchase.SalesDesc = node.SelectSingleNode("SalesDesc") == null ? "" : node.SelectSingleNode("SalesDesc").InnerText;
+					salesAndPurchase.SalesPrice = node.SelectSingleNode("SalesPrice") == null ? 0 : decimal.Parse(node.SelectSingleNode("SalesPrice").InnerText);
+					salesAndPurchase.PurchaseDesc = node.SelectSingleNode("PurchaseDesc") == null ? "" : node.SelectSingleNode("PurchaseDesc").InnerText;
+					salesAndPurchase.PurchaseCost = node.SelectSingleNode("PurchaseCost") == null ? 0 : decimal.Parse(node.SelectSingleNode("PurchaseCost").InnerText);
+					subNode = node.SelectSingleNode("IncomeAccountRef");
+
+					if (subNode != null) {
+						salesAndPurchase.IncomeAccountRef.ListID = subNode.SelectSingleNode("ListID") == null ? "" : subNode.SelectSingleNode("ListID").InnerText;
+						salesAndPurchase.IncomeAccountRef.FullName = subNode.SelectSingleNode("FullName") == null ? "" : subNode.SelectSingleNode("FullName").InnerText;
+					}
+
+					subNode = node.SelectSingleNode("PurchaseTaxCodeRef");
+
+					if (subNode != null) {
+						salesAndPurchase.PurchaseTaxCodeRef.ListID = subNode.SelectSingleNode("ListID") == null ? "" : subNode.SelectSingleNode("ListID").InnerText;
+						salesAndPurchase.PurchaseTaxCodeRef.FullName = subNode.SelectSingleNode("FullName") == null ? "" : subNode.SelectSingleNode("FullName").InnerText;
+					}
+
+					subNode = node.SelectSingleNode("ExpenseAccountRef");
+
+					if (subNode != null) {
+						salesAndPurchase.ExpenseAccountRef.ListID = subNode.SelectSingleNode("ListID") == null ? "" : subNode.SelectSingleNode("ListID").InnerText;
+						salesAndPurchase.ExpenseAccountRef.FullName = subNode.SelectSingleNode("FullName") == null ? "" : subNode.SelectSingleNode("FullName").InnerText;
+					}
+
+					subNode = node.SelectSingleNode("PrefVendorRef");
+
+					if (subNode != null) {
+						salesAndPurchase.PrefVendorRef.ListID = subNode.SelectSingleNode("ListID") == null ? "" : subNode.SelectSingleNode("ListID").InnerText;
+						salesAndPurchase.PrefVendorRef.FullName = subNode.SelectSingleNode("FullName") == null ? "" : subNode.SelectSingleNode("FullName").InnerText;
+					}
+				}
+				#endregion Get Subnodes
+
+				return new ItemNonInventoryRet {
+					ListID = xml.SelectNodes("ListID")[0] == null ? "" : xml.SelectNodes("ListID")[0].InnerText,
+					TimeCreated = DateTime.Parse(xml.SelectNodes("TimeCreated")[0].InnerText),
+					TimeModified = DateTime.Parse(xml.SelectNodes("TimeModified")[0].InnerText),
+					EditSequence = xml.SelectNodes("EditSequence")[0] == null ? "" : xml.SelectNodes("EditSequence")[0].InnerText,
+					Name = xml.SelectNodes("Name")[0] == null ? "" : xml.SelectNodes("Name")[0].InnerText,
+					FullName = xml.SelectNodes("FullName")[0] == null ? "" : xml.SelectNodes("FullName")[0].InnerText,
+					BarCodeValue = xml.SelectNodes("BarCodeValue")[0] == null ? "" : xml.SelectNodes("BarCodeValue")[0].InnerText,
+					IsActive = xml.SelectNodes("IsActive")[0] == null ? false : bool.Parse(xml.SelectNodes("IsActive")[0].InnerText),
+					ClassRef = classRef,
+					ParentRef = parentRef,
+					Sublevel = xml.SelectNodes("Sublevel")[0] == null ? 0 : int.Parse(xml.SelectNodes("Sublevel")[0].InnerText),
+					ManufacturerPartNumber = xml.SelectNodes("ManufacturerPartNumber")[0] == null ? "" : xml.SelectNodes("ManufacturerPartNumber")[0].InnerText,
+					UnitOfMeasureSetRef = unitOfMeasureSetRef,
+					IsTaxIncluded = xml.SelectNodes("IsTaxIncluded")[0] == null ? false : bool.Parse(xml.SelectNodes("IsTaxIncluded")[0].InnerText),
+					SalesTaxCodeRef = salesTaxCodeRef,
+					SalesOrPurchase = salesOrPurchase,
+					SalesAndPurchase = salesAndPurchase,
+					ExternalGUID = xml.SelectNodes("ExternalGUID")[0] == null ? "" : xml.SelectNodes("ExternalGUID")[0].InnerText,
+				};
+			}
+
+			catch (Exception ex) {
+				string msg;
+
+				if (ex.InnerException == null)
+					msg = String.Format("{0}{2}Exception thrown in Qbfc13Util.XmlNodeToItemNonInventoryRet(XmlNode xml).{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine);
+				else
+					msg = String.Format("{0}{2}Exception thrown in INNER EXCEPTION of Qbfc13Util.XmlNodeToItemNonInventoryRet(XmlNode xml).{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine);
+
+				if (_logLevel <= LogLevel.Error)
+					Console.Write("\n[{0}] {1}", DateTime.Now.ToString(TIMESTAMP), msg);
+
+				throw new Exception(msg);
+			}
 		}
 
 		//public void DoItemNonInventoryQuery()
