9e773ce77fa6352d144156ec331506a73a0cd59d
blizmanyu <blizman2017@gmail.com>
2019-07-08 14:14:53 -0700

    Added more classes and organized

--- Libraries/Yutaka.QuickBooks/HelperClasses.cs
+++ Libraries/Yutaka.QuickBooks/HelperClasses.cs
@@ -1,41 +1,98 @@
 ﻿namespace Yutaka.QuickBooks
 {
+	#region public class AccountRef
 	public class AccountRef
 	{
 		public string ListID;
 		public string FullName;
 	}
+	#endregion AccountRef
 
+	#region public class ClassRef
 	public class ClassRef
 	{
 		public string ListID;
 		public string FullName;
 	}
+	#endregion ClassRef
 
+	#region public class ExpenseAccountRef
+	public class ExpenseAccountRef
+	{
+		public string ListID;
+		public string FullName;
+	}
+	#endregion ExpenseAccountRef
+
+	#region public class IncomeAccountRef
+	public class IncomeAccountRef
+	{
+		public string ListID;
+		public string FullName;
+	}
+	#endregion IncomeAccountRef
+
+	#region public class ParentRef
 	public class ParentRef
 	{
 		public string ListID;
 		public string FullName;
 	}
+	#endregion ParentRef
 
-	public class UnitOfMeasureSetRef
+	#region public class PrefVendorRef
+	public class PrefVendorRef
 	{
 		public string ListID;
 		public string FullName;
 	}
+	#endregion PrefVendorRef
 
+	#region public class PurchaseTaxCodeRef
+	public class PurchaseTaxCodeRef
+	{
+		public string ListID;
+		public string FullName;
+	}
+	#endregion PurchaseTaxCodeRef
+
+	#region public class SalesAndPurchase
+	public class SalesAndPurchase
+	{
+		public IncomeAccountRef IncomeAccountRef = new IncomeAccountRef();
+		public PurchaseTaxCodeRef PurchaseTaxCodeRef = new PurchaseTaxCodeRef();
+		public ExpenseAccountRef ExpenseAccountRef = new ExpenseAccountRef();
+		public PrefVendorRef PrefVendorRef = new PrefVendorRef();
+		public string SalesDesc;
+		public string PurchaseDesc;
+		public decimal SalesPrice;
+		public decimal PurchaseCost;
+	}
+	#endregion SalesAndPurchase
+
+	#region public class SalesOrPurchase
+	public class SalesOrPurchase
+	{
+		public AccountRef AccountRef = new AccountRef();
+		public string Desc;
+		public decimal Price;
+		public decimal PricePercent;
+	}
+	#endregion SalesOrPurchase
+
+	#region public class SalesTaxCodeRef
 	public class SalesTaxCodeRef
 	{
 		public string ListID;
 		public string FullName;
 	}
+	#endregion SalesTaxCodeRef
 
-	public class SalesOrPurchase
+	#region public class UnitOfMeasureSetRef
+	public class UnitOfMeasureSetRef
 	{
-		public AccountRef AccountRef;
-		public string Desc;
-		public decimal Price;
-		public decimal PricePercent;
-		public SalesOrPurchase() { AccountRef = new AccountRef(); }
+		public string ListID;
+		public string FullName;
 	}
+	#endregion UnitOfMeasureSetRef
 }
