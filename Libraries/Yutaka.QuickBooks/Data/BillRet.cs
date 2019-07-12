2db8eaca9951fa2559ef44e7a93099f8b9a94242
blizmanyu <blizman2017@gmail.com>
2019-07-12 16:14:33 -0700

    New class BillRet (WIP)

---
+++ Libraries/Yutaka.QuickBooks/Data/BillRet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Yutaka.QuickBooks
+{
+	public class BillRet
+	{
+		public string TxnID; // required
+		public DateTime TimeCreated; // required
+		public DateTime TimeModified; // required
+		public string EditSequence; // required
+		public int? TxnNumber;
+		public VendorRef VendorRef;
+		public VendorAddress VendorAddress;
+		public APAccountRef APAccountRef;
+		public DateTime TxnDate; // required
+		public DateTime? DueDate;
+		public decimal AmountDue; // required
+		public CurrencyRef CurrencyRef;
+		public decimal? ExchangeRate;
+		public decimal? AmountDueInHomeCurrency;
+		public string RefNumber;
+		public TermsRef TermsRef;
+		public string Memo;
+		public bool? IsTaxIncluded;
+		public SalesTaxCodeRef SalesTaxCodeRef;
+		public bool? IsPaid;
+		public string ExternalGUID;
+		public object laskfjdlskdjf;
+		//public object laskfjdlskdjf;
+		//public object laskfjdlskdjf;
+		//public object laskfjdlskdjf;
+		//public object laskfjdlskdjf;
+		//public object laskfjdlskdjf;
+		//public object laskfjdlskdjf;
+
+		public BillRet()
+		{
+
+		}
+	}
+}
