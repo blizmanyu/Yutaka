858195e392e21de10b951a106fc054d81d0a8952
blizmanyu <blizman2017@gmail.com>
2019-07-15 16:48:53 -0700

    Finished version 1

--- Libraries/Yutaka.QuickBooks/Data/Bill_ExpenseLineRet.cs
+++ Libraries/Yutaka.QuickBooks/Data/Bill_ExpenseLineRet.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Yutaka.QuickBooks
+﻿namespace Yutaka.QuickBooks
 {
 	public class Bill_ExpenseLineRet
 	{
+		public string TxnID; // required
+		public string TxnLineID; // required
+		public AccountRef AccountRef;
+		public string Memo;
+		public CustomerRef CustomerRef;
+		public ClassRef ClassRef;
+		public SalesTaxCodeRef SalesTaxCodeRef;
+		public string BillableStatus;
+		public SalesRepRef SalesRepRef;
+
+		public Bill_ExpenseLineRet()
+		{
+			AccountRef = new AccountRef();
+			CustomerRef = new CustomerRef();
+			ClassRef = new ClassRef();
+			SalesTaxCodeRef = new SalesTaxCodeRef();
+			SalesRepRef = new SalesRepRef();
+		}
 	}
 }
