9b07cf9b928aa4d97b32933ba67291b7329831c5
blizmanyu <blizman2017@gmail.com>
2019-07-17 17:44:13 -0700

    Removed unused Usings. New method public BillRet XmlNodeToBillRet(XmlNode xml)

--- Libraries/Yutaka.QuickBooks/Qbfc13Util.cs
+++ Libraries/Yutaka.QuickBooks/Qbfc13Util.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Net;
-using System.Drawing;
-using System.Collections;
-using System.ComponentModel;
-using System.Data;
-using System.IO;
+using System.Collections.Generic;
+using System.Xml;
 using Interop.QBFC13;
 using Interop.QBXMLRP2Lib;
-using System.Xml;
 
 namespace Yutaka.QuickBooks
 {
@@ -162,7 +157,182 @@ namespace Yutaka.QuickBooks
 				return "";
 
 			return dt.Value.ToString(QB_FORMAT);
-		} 
+		}
+
+		public BillRet XmlNodeToBillRet(XmlNode xml)
+		{
+			if (_logLevel <= LogLevel.Trace)
+				Console.Write("\n[{0}] Begin method XmlNodeToBillRet(XmlNode xml).", DateTime.Now.ToString(TIMESTAMP));
+			if (xml == null || !xml.HasChildNodes)
+				throw new Exception("<xml> is required. Exception thrown in Qbfc13Util.XmlNodeToBillRet(XmlNode xml).{0}{0}");
+
+			try {
+				XmlNodeList retList;
+				XmlNode node, subNode;
+				var TxnID = xml.SelectSingleNode("TxnID") == null ? "" : xml.SelectSingleNode("TxnID").InnerText;
+				var VendorRef = new VendorRef();
+				var VendorAddress = new VendorAddress();
+				var APAccountRef = new APAccountRef();
+				var CurrencyRef = new CurrencyRef();
+				var TermsRef = new TermsRef();
+				var SalesTaxCodeRef = new SalesTaxCodeRef();
+				var ExpenseLines = new List<Bill_ExpenseLineRet>();
+
+				#region Get Subnodes
+				node = xml.SelectSingleNode("VendorRef");
+
+				if (node != null) {
+					VendorRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					VendorRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				node = xml.SelectSingleNode("VendorAddress");
+
+				if (node != null) {
+					VendorAddress.Addr1 = node.SelectSingleNode("Addr1") == null ? "" : node.SelectSingleNode("Addr1").InnerText;
+					VendorAddress.Addr2 = node.SelectSingleNode("Addr2") == null ? "" : node.SelectSingleNode("Addr2").InnerText;
+					VendorAddress.Addr3 = node.SelectSingleNode("Addr3") == null ? "" : node.SelectSingleNode("Addr3").InnerText;
+					VendorAddress.Addr4 = node.SelectSingleNode("Addr4") == null ? "" : node.SelectSingleNode("Addr4").InnerText;
+					VendorAddress.Addr5 = node.SelectSingleNode("Addr5") == null ? "" : node.SelectSingleNode("Addr5").InnerText;
+					VendorAddress.City = node.SelectSingleNode("City") == null ? "" : node.SelectSingleNode("City").InnerText;
+					VendorAddress.State = node.SelectSingleNode("State") == null ? "" : node.SelectSingleNode("State").InnerText;
+					VendorAddress.PostalCode = node.SelectSingleNode("PostalCode") == null ? "" : node.SelectSingleNode("PostalCode").InnerText;
+					VendorAddress.Country = node.SelectSingleNode("Country") == null ? "" : node.SelectSingleNode("Country").InnerText;
+					VendorAddress.Note = node.SelectSingleNode("Note") == null ? "" : node.SelectSingleNode("Note").InnerText;
+				}
+
+				node = xml.SelectSingleNode("APAccountRef");
+
+				if (node != null) {
+					APAccountRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					APAccountRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				node = xml.SelectSingleNode("CurrencyRef");
+
+				if (node != null) {
+					CurrencyRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					CurrencyRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				node = xml.SelectSingleNode("TermsRef");
+
+				if (node != null) {
+					TermsRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					TermsRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				node = xml.SelectSingleNode("SalesTaxCodeRef");
+
+				if (node != null) {
+					SalesTaxCodeRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+					SalesTaxCodeRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+				}
+
+				AccountRef AccountRef;
+				CustomerRef CustomerRef;
+				ClassRef ClassRef;
+				SalesTaxCodeRef SalesTaxCodeRefSub;
+				SalesRepRef SalesRepRef;
+				retList = xml.SelectNodes("ExpenseLineRet");
+
+				foreach (XmlNode ret in retList) {
+					AccountRef = new AccountRef();
+					node = ret.SelectSingleNode("AccountRef");
+
+					if (node != null) {
+						AccountRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+						AccountRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+					}
+
+					CustomerRef = new CustomerRef();
+					node = ret.SelectSingleNode("CustomerRef");
+
+					if (node != null) {
+						CustomerRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+						CustomerRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+					}
+
+					ClassRef = new ClassRef();
+					node = ret.SelectSingleNode("ClassRef");
+
+					if (node != null) {
+						ClassRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+						ClassRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+					}
+
+					SalesTaxCodeRefSub = new SalesTaxCodeRef();
+					node = ret.SelectSingleNode("SalesTaxCodeRef");
+
+					if (node != null) {
+						SalesTaxCodeRefSub.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+						SalesTaxCodeRefSub.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+					}
+
+					SalesRepRef = new SalesRepRef();
+					node = ret.SelectSingleNode("SalesRepRef");
+
+					if (node != null) {
+						SalesRepRef.ListID = node.SelectSingleNode("ListID") == null ? "" : node.SelectSingleNode("ListID").InnerText;
+						SalesRepRef.FullName = node.SelectSingleNode("FullName") == null ? "" : node.SelectSingleNode("FullName").InnerText;
+					}
+
+					ExpenseLines.Add(new Bill_ExpenseLineRet {
+						TxnID = TxnID,
+						TxnLineID = ret.SelectSingleNode("TxnLineID") == null ? "" : ret.SelectSingleNode("TxnLineID").InnerText,
+						AccountRef = AccountRef,
+						Amount = ret.SelectSingleNode("Amount") == null ? (decimal?) null : decimal.Parse(ret.SelectSingleNode("Amount").InnerText),
+						Memo = ret.SelectSingleNode("Memo") == null ? "" : ret.SelectSingleNode("Memo").InnerText,
+						CustomerRef = CustomerRef,
+						ClassRef = ClassRef,
+						SalesTaxCodeRef = SalesTaxCodeRefSub,
+						BillableStatus = ret.SelectSingleNode("BillableStatus") == null ? "" : ret.SelectSingleNode("BillableStatus").InnerText,
+						SalesRepRef = SalesRepRef,
+					});
+				}
+				#endregion Get Subnodes
+
+				return new BillRet {
+					TxnID = TxnID ?? "",
+					TimeCreated = DateTime.Parse(xml.SelectSingleNode("TimeCreated").InnerText),
+					TimeModified = DateTime.Parse(xml.SelectSingleNode("TimeModified").InnerText),
+					EditSequence = xml.SelectSingleNode("EditSequence") == null ? "" : xml.SelectSingleNode("EditSequence").InnerText,
+					TxnNumber = xml.SelectSingleNode("TxnNumber") == null ? (int?) null : int.Parse(xml.SelectSingleNode("TxnNumber").InnerText),
+					VendorRef = VendorRef,
+					VendorAddress = VendorAddress,
+					APAccountRef = APAccountRef,
+					TxnDate = DateTime.Parse(xml.SelectSingleNode("TxnDate").InnerText),
+					DueDate = xml.SelectSingleNode("DueDate") == null ? (DateTime?) null : DateTime.Parse(xml.SelectSingleNode("DueDate").InnerText),
+					AmountDue = decimal.Parse(xml.SelectSingleNode("AmountDue").InnerText),
+					CurrencyRef = CurrencyRef,
+					ExchangeRate = xml.SelectSingleNode("ExchangeRate") == null ? (decimal?) null : decimal.Parse(xml.SelectSingleNode("ExchangeRate").InnerText),
+					AmountDueInHomeCurrency = xml.SelectSingleNode("AmountDueInHomeCurrency") == null ? (decimal?) null : decimal.Parse(xml.SelectSingleNode("AmountDueInHomeCurrency").InnerText),
+					RefNumber = xml.SelectSingleNode("RefNumber") == null ? "" : xml.SelectSingleNode("RefNumber").InnerText,
+					TermsRef = TermsRef,
+					Memo = xml.SelectSingleNode("Memo") == null ? "" : xml.SelectSingleNode("Memo").InnerText,
+					IsTaxIncluded = xml.SelectSingleNode("IsTaxIncluded") == null ? (bool?) null : bool.Parse(xml.SelectSingleNode("IsTaxIncluded").InnerText),
+					SalesTaxCodeRef = SalesTaxCodeRef,
+					IsPaid = xml.SelectSingleNode("IsPaid") == null ? (bool?) null : bool.Parse(xml.SelectSingleNode("IsPaid").InnerText),
+					ExternalGUID = xml.SelectSingleNode("ExternalGUID") == null ? "" : xml.SelectSingleNode("ExternalGUID").InnerText,
+					OpenAmount = xml.SelectSingleNode("OpenAmount") == null ? (decimal?) null : decimal.Parse(xml.SelectSingleNode("OpenAmount").InnerText),
+					ExpenseLines = ExpenseLines,
+				};
+			}
+
+			catch (Exception ex) {
+				string msg;
+
+				if (ex.InnerException == null)
+					msg = String.Format("{0}{2}Exception thrown in Qbfc13Util.XmlNodeToBillRet(XmlNode xml).{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine);
+				else
+					msg = String.Format("{0}{2}Exception thrown in INNER EXCEPTION of Qbfc13Util.XmlNodeToBillRet(XmlNode xml).{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine);
+
+				if (_logLevel <= LogLevel.Error)
+					Console.Write("\n[{0}] {1}", DateTime.Now.ToString(TIMESTAMP), msg);
+
+				throw new Exception(msg);
+			}
+		}
 
 		public ItemNonInventoryRet XmlNodeToItemNonInventoryRet(XmlNode xml)
 		{
