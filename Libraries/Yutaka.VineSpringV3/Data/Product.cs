76accd66cbf987dccb2549d3be6d3ef1285b93c1
blizmanyu <blizman2017@gmail.com>
2019-08-27 11:23:01 -0700

    New field bool? soldOut

--- Libraries/Yutaka.VineSpringV3/Data/Product.cs
+++ Libraries/Yutaka.VineSpringV3/Data/Product.cs
@@ -16,6 +16,7 @@ namespace Yutaka.VineSpringV3
 		public string name { get; set; }
 		public string[] tags { get; set; }
 		public string detailImage { get; set; }
+		public bool? soldOut { get; set; }
 	}
 
 	public class Visibility
