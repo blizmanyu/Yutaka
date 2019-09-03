c58ed2d0c27321fff5d10ee18b2c5619d88cb3b7
blizmanyu <blizman2017@gmail.com>
2019-09-03 12:22:55 -0700

    Constructor

--- Libraries/Yutaka.WineShipping/WSUtil.cs
+++ Libraries/Yutaka.WineShipping/WSUtil.cs
@@ -17,5 +17,24 @@ namespace Yutaka.WineShipping
 		public string CustomerNumber;
 		#endregion Fields
 
+		#region Constructor
+		public WSUtil(string baseUrl=null, string userKey=null, string password=null, string customerNumber=null)
+		{
+			if (String.IsNullOrWhiteSpace(baseUrl))
+				baseUrl = PRODUCTION_URL;
+			if (String.IsNullOrWhiteSpace(userKey))
+				throw new Exception(String.Format("<userKey> is required. Exception thrown in Constructor WSUtil(string baseUrl, string userKey, string password, string customerNumber).{0}", Environment.NewLine));
+			if (String.IsNullOrWhiteSpace(password))
+				throw new Exception(String.Format("<password> is required. Exception thrown in Constructor WSUtil(string baseUrl, string userKey, string password, string customerNumber).{0}", Environment.NewLine));
+			if (String.IsNullOrWhiteSpace(customerNumber))
+				throw new Exception(String.Format("<customerNumber> is required. Exception thrown in Constructor WSUtil(string baseUrl, string userKey, string password, string customerNumber).{0}", Environment.NewLine));
+
+			BaseUrl = new Uri(baseUrl);
+			UserKey = userKey;
+			Password = password;
+			CustomerNumber = customerNumber;
+		}
+		#endregion Constructor
+
 	}
 }
