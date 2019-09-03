f6ebcb740581e4420bc636749bbbe4e2891111e8
blizmanyu <blizman2017@gmail.com>
2019-09-03 15:40:20 -0700

    New class InventoryStatusResult & WarehouseInventoryStatus

---
+++ Libraries/Yutaka.WineShipping/Data/InventoryStatusResult.cs
@@ -0,0 +1,27 @@
+﻿namespace Yutaka.WineShipping
+{
+	public class InventoryStatusResult
+	{
+		public int? TotalRecordCount { get; set; }
+		public int? Skip { get; set; }
+		public int? Top { get; set; }
+		public bool? MoreRecords { get; set; }
+		public WarehouseInventoryStatus[] WarehouseInventoryStatuses { get; set; }
+	}
+
+	public class WarehouseInventoryStatus
+	{
+		public string CustomerNo { get; set; }
+		public string CustomerName { get; set; }
+		public string ItemNo { get; set; }
+		public string ItemDescription { get; set; }
+		public string ItemUnit { get; set; }
+		public string InventoryWarehouse { get; set; }
+		public string InventoryStatus { get; set; }
+		public int? OnHandQuantity { get; set; }
+		public int? ReservedQuantity { get; set; }
+		public int? AvailableQuantity { get; set; }
+		public int? BackOrderQuantity { get; set; }
+		public int? OpenPOQuantity { get; set; }
+	}
+}
