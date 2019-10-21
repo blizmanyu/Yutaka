49546c0332d73f62a8785b552ac2fb2beb8fcd58
Yutaka Blizman <blizman2017@gmail.com>
2019-10-21 15:18:17 -0700

    New public class QB20191021Util

---
+++ Libraries/Yutaka.QuickBooks/QB20191021Util.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interop.QBXMLRP2Lib;
+
+namespace Yutaka.QuickBooks
+{
+	public class QB20191021Util
+	{
+		#region Fields
+		public const string DEFAULT_APP_NAME = "QB20191021Util";
+		public bool Debug;
+		private RequestProcessor2 Rp;
+		private bool ConnectionOpen;
+		private bool SessionBegun;
+		private string SessionId;
+		#endregion Fields
+
+		public QB20191021Util()
+		{
+			Debug = false;
+			Rp = null;
+			ConnectionOpen = false;
+			SessionBegun = false;
+			SessionId = null;
+		}
+
+		#region Methods
+		public void CloseConnection()
+		{
+			if (SessionBegun) {
+				Rp.EndSession(SessionId);
+				SessionBegun = false;
+			}
+
+			if (ConnectionOpen) {
+				Rp.CloseConnection();
+				ConnectionOpen = false;
+			}
+		}
+
+		public bool OpenConnection(string appId = null, string appName = null, QBXMLRPConnectionType connPref = QBXMLRPConnectionType.localQBD, string qbFileName = null, QBFileMode reqFileMode = QBFileMode.qbFileOpenDoNotCare)
+		{
+			#region Input Validation
+			if (String.IsNullOrWhiteSpace(appId))
+				appId = DEFAULT_APP_NAME;
+			if (String.IsNullOrWhiteSpace(appName))
+				appName = DEFAULT_APP_NAME;
+			if (connPref < 0)
+				connPref = QBXMLRPConnectionType.localQBD;
+			if (String.IsNullOrWhiteSpace(qbFileName))
+				qbFileName = "";
+			if (reqFileMode < 0)
+				reqFileMode = QBFileMode.qbFileOpenDoNotCare;
+			#endregion Input Validation
+
+			try {
+				Rp = new RequestProcessor2();
+				Rp.OpenConnection2(appId, appName, connPref);
+				ConnectionOpen = true;
+				SessionId = Rp.BeginSession(qbFileName, reqFileMode);
+				SessionBegun = true;
+				return true;
+			}
+
+			catch (Exception ex) {
+				#region Log
+				string log;
+
+				if (ex.InnerException == null)
+					log = String.Format("{0}{2}Exception thrown in QBV20191021Util.OpenConnection(string appId='{3}', string appName='{4}', QBXMLRPConnectionType connPref='{5}', string qbFileName='{6}', QBFileMode reqFileMode='{7}').{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, appId, appName, connPref.ToString(), qbFileName, reqFileMode.ToString());
+				else
+					log = String.Format("{0}{2}Exception thrown in INNER EXCEPTION of QBV20191021Util.OpenConnection(string appId='{3}', string appName='{4}', QBXMLRPConnectionType connPref='{5}', string qbFileName='{6}', QBFileMode reqFileMode='{7}').{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, appId, appName, connPref.ToString(), qbFileName, reqFileMode.ToString());
+
+				if (Debug)
+					Console.Write("\n{0}", log);
+				#endregion Log
+
+				if (SessionBegun) {
+					Rp.EndSession(SessionId);
+					SessionBegun = false;
+				}
+
+				if (ConnectionOpen) {
+					Rp.CloseConnection();
+					ConnectionOpen = false;
+				}
+
+				return false;
+			}
+		}
+		#endregion Methods
+	}
+}
