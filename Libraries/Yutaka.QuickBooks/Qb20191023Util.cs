871df51a78c69c3e0205f9d90624b82339e8d726
Yutaka Blizman <blizman2017@gmail.com>
2019-10-23 14:12:08 -0700

    New public class Qb20191023Util

---
+++ Libraries/Yutaka.QuickBooks/Qb20191023Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yutaka.QuickBooks
+{
+	public class Qb20191023Util
+	{
+	}
+}
