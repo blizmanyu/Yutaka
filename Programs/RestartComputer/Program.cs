004f661a53cbfdb434404e26ceefcf0551c98e6f
Yutaka Blizman <blizman2017@gmail.com>
2019-11-12 17:24:23 -0800

    Done version 1

--- Programs/RestartComputer/Program.cs
+++ Programs/RestartComputer/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Yutaka.Diagnostics;
 
 namespace RestartComputer
 {
 	class Program
 	{
+		private static string[] programs = new string[] { "Dynamics", "OUTLOOK", "Skype", };
+
 		static void Main(string[] args)
 		{
+			if (ProcessUtil.GetUpTime() > ProcessUtil.FIVE_DAYS_IN_SECONDS) {
+				var killCount = 0;
+				var closeCount = 0;
+
+				foreach (var program in programs) {
+					if (ProcessUtil.CloseProgram(program) > 0) {
+						Console.Write("\nClosed {0}", program);
+						closeCount++;
+					}
+				}
+
+				foreach (var program in programs) {
+					if (ProcessUtil.KillProcess(program) > 0) {
+						Console.Write("\nKilled {0}", program);
+						killCount++;
+					}
+				}
+
+				Console.Write("\nClosed {0} programs and killed {1}", closeCount, killCount);
+				ProcessUtil.RestartComputer();
+			}
 		}
 	}
 }
