94dd401bb49aba5efccfbf67d7fcd4e624d2df31
blizmanyu <blizman2017@gmail.com>
2019-11-15 01:18:19 -0800

    Added fields and Constructor (WIP)

--- Libraries/Yutaka.Web/YuSession.cs
+++ Libraries/Yutaka.Web/YuSession.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.SessionState;
 
 namespace Yutaka.Web
 {
 	public class YuSession
 	{
+		public DateTime CreatedOnUtc;
+		public string Ip;
+		public string UniqueId;
+		protected HttpRequest Request;
+		protected HttpSessionState Session;
+
+		public YuSession()
+		{
+			CreatedOnUtc = DateTime.UtcNow;
+			Ip = HttpContext.Current.Request.UserHostAddress ?? "";
+		}
 	}
 }
