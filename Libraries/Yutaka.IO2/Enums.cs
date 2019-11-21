32d17512e36087c1055779767f7db9b93c893c2b
Yutaka Blizman <blizman2017@gmail.com>
2019-11-21 14:25:01 -0800

    Renamed OverwriteIfDifferent to Smart

--- Libraries/Yutaka.IO2/Enums.cs
+++ Libraries/Yutaka.IO2/Enums.cs
@@ -1,4 +1,16 @@
 ﻿namespace Yutaka.IO2
 {
-	public enum OverwriteOption { Overwrite, Skip, KeepBoth, OverwriteIfDifferent, }
+	/// <summary>
+	/// Specifies whether to overwrite, skip, keep both, or use the "smart" algorithm.
+	/// </summary>
+	public enum OverwriteOption
+	{
+		Overwrite,
+		Skip,
+		KeepBoth,
+		/// <summary>
+		/// If the files are the same, it will skip to save resources. Otherwise, it will rename to keep both files.
+		/// </summary>
+		Smart
+	}
 }
