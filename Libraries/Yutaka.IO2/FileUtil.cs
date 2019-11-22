67ce74093ae0caecffd7e26635a61322a7f4340a
blizmanyu <blizman2017@gmail.com>
2019-11-22 01:27:59 -0800

    New method public static int Delete(string folderPath, string searchPattern = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly)

--- Libraries/Yutaka.IO2/FileUtil.cs
+++ Libraries/Yutaka.IO2/FileUtil.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace Yutaka.IO2
 {
@@ -187,6 +188,38 @@ namespace Yutaka.IO2
 				throw new Exception(log);
 				#endregion Log
 			}
+		}
+
+		/// <summary>
+		/// Deletes all files that match a search pattern in a specified path, and optionally searches subdirectories. Returns the number of files deleted.
+		/// </summary>
+		/// <param name="folderPath">The relative or absolute path to the directory to search. This string is not case-sensitive.</param>
+		/// <param name="searchPattern">The search string to match against the names of files in &lt;folderPath&gt;. This parameter can contain a combination of valid literal path and wildcard (* and ?) characters, but it doesn't support regular expressions.</param>
+		/// <param name="searchOption">One of the enumeration values that specifies whether the search operation should include only the current directory or should include all subdirectories.</param>
+		/// <returns>The number of files deleted.</returns>
+		public static int Delete(string folderPath, string searchPattern = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly)
+		{
+			#region Input Check
+			if (String.IsNullOrWhiteSpace(folderPath))
+				return 0;
+			if (!Directory.Exists(folderPath))
+				return 0;
+			if (String.IsNullOrWhiteSpace(searchPattern))
+				searchPattern = "*";
+			#endregion Input Check
+
+			var count = 0;
+
+			Directory.EnumerateFiles(folderPath, searchPattern, searchOption).AsParallel().ForAll(path => {
+				try {
+					File.Delete(path);
+					count++;
+				}
+
+				catch (Exception) { }
+			});
+
+			return count;
 		}
 
 		/// <summary>
