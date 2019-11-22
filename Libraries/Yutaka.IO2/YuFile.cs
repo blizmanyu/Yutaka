d38b5b52cefe66bd663a838cef2dce346cb934ab
Yutaka Blizman <blizman2017@gmail.com>
2019-11-21 19:16:36 -0800

    New method public bool CopyTo(string destFileName, OverwriteOption overwriteOption = OverwriteOption.Skip)

--- Libraries/Yutaka.IO2/YuFile.cs
+++ Libraries/Yutaka.IO2/YuFile.cs
@@ -79,17 +79,16 @@ namespace Yutaka.IO2
 		/// <summary>
 		/// Fast file copy with big buffers. If &lt;destFileName&gt; exists, it will be overwritten.
 		/// </summary>
-		/// <param name="sourceFileName">The file to copy.</param>
 		/// <param name="destFileName">The name of the destination file. This cannot be a directory.</param>
 		/// <seealso cref="https://www.codeproject.com/Tips/777322/A-Faster-File-Copy"/>
-		protected static bool FastCopy(string sourceFileName, string destFileName)
+		protected bool FastCopyTo(string destFileName)
 		{
 			int read;
 			var array_length = FIVE_HUNDRED_TWELVE_KB;
 			var dataArray = new byte[array_length];
 
 			try {
-				using (var fsread = new FileStream(sourceFileName, FileMode.Open, FileAccess.Read, FileShare.None, array_length)) {
+				using (var fsread = new FileStream(FullName, FileMode.Open, FileAccess.Read, FileShare.None, array_length)) {
 					using (var bwread = new BinaryReader(fsread)) {
 						using (var fswrite = new FileStream(destFileName, FileMode.Create, FileAccess.Write, FileShare.None, array_length)) {
 							using (var bwwrite = new BinaryWriter(fswrite)) {
@@ -112,9 +111,9 @@ namespace Yutaka.IO2
 				string log;
 
 				if (ex.InnerException == null)
-					log = String.Format("{0}{2}Exception thrown in YuFile.FastCopy(string sourceFileName='{3}', string destFileName='{4}'){2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, sourceFileName, destFileName);
+					log = String.Format("{0}{2}Exception thrown in YuFile.FastCopyTo(string destFileName='{3}'){2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, destFileName);
 				else
-					log = String.Format("{0}{2}Exception thrown in INNER EXCEPTION of YuFile.FastCopy(string sourceFileName='{3}', string destFileName='{4}'){2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, sourceFileName, destFileName);
+					log = String.Format("{0}{2}Exception thrown in INNER EXCEPTION of YuFile.FastCopyTo(string destFileName='{3}'){2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, destFileName);
 
 				Console.Write("\n{0}", log);
 				#endregion Log
@@ -125,6 +124,83 @@ namespace Yutaka.IO2
 		#endregion Utilities
 
 		#region Methods
+		/// <summary>
+		/// Copies an existing file to a new file.
+		/// </summary>
+		/// <param name="destFileName">The name of the new file to copy to.</param>
+		/// <param name="overwriteOption">The <see cref="OverwriteOption"/> to use.</param>
+		/// <returns></returns>
+		public bool CopyTo(string destFileName, OverwriteOption overwriteOption = OverwriteOption.Skip)
+		{
+			#region Input Check
+			var log = "";
+
+			if (String.IsNullOrWhiteSpace(destFileName))
+				log = String.Format("{0}<destFileName> is required.{1}", log, Environment.NewLine);
+			if (FullName.ToUpper().Equals(destFileName.ToUpper()))
+				log = String.Format("{0}<this> and <destFileName> are the same.{1}", log, Environment.NewLine);
+
+			if (!String.IsNullOrWhiteSpace(log)) {
+				log = String.Format("{0}Exception thrown in YuFile.CopyTo(string destFileName, OverwriteOption overwriteOption).{1}", log, Environment.NewLine);
+				Console.Write("\n{0}\n", log);
+				return false;
+			}
+			#endregion Input Check
+
+			try {
+				var destFileExists = File.Exists(destFileName);
+
+				switch (overwriteOption) {
+					#region case OverwriteOption.Overwrite:
+					case OverwriteOption.Overwrite:
+						return FastCopyTo(destFileName);
+					#endregion
+					#region case OverwriteOption.Skip:
+					case OverwriteOption.Skip:
+						if (destFileExists)
+							return false;
+
+						return FastCopyTo(destFileName);
+					#endregion
+					#region case OverwriteOption.KeepBoth:
+					case OverwriteOption.KeepBoth:
+						if (destFileExists)
+							return FastCopyTo(FileUtil.AutoRename(destFileName));
+
+						return FastCopyTo(destFileName);
+					#endregion
+					#region case OverwriteOption.Smart:
+					case OverwriteOption.Smart:
+						if (destFileExists) {
+							if (Size == new FileInfo(destFileName).Length)
+								return true;
+
+							return FastCopyTo(FileUtil.AutoRename(destFileName));
+						}
+
+						return FastCopyTo(destFileName);
+					#endregion
+					default:
+						return false;
+				}
+			}
+
+			catch (Exception ex) {
+				#region Log
+				log = "";
+
+				if (ex.InnerException == null)
+					log = String.Format("{0}{2}Exception thrown in YuFile.CopyTo(string destFileName='{3}', OverwriteOption overwriteOption='{4}'){2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, destFileName, overwriteOption.ToString());
+				else
+					log = String.Format("{0}{2}Exception thrown in INNER EXCEPTION of YuFile.CopyTo(string destFileName='{3}', OverwriteOption overwriteOption='{4}'){2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, destFileName, overwriteOption.ToString());
+
+				Console.Write("\n{0}", log);
+				#endregion Log
+
+				return false;
+			}
+		}
+
 		#region Overrides
 		public override bool Equals(Object obj)
 		{
