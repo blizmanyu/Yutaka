e3cdf163247e616ca1924862b4b7feccb3f375cb
Yutaka Blizman <blizman2017@gmail.com>
2019-11-26 14:36:44 -0800

    Changed bool RequireShippingAddress to bool?

--- Libraries/Yutaka.VineSpringV3/Data20191126/ShippingMethod.cs
+++ Libraries/Yutaka.VineSpringV3/Data20191126/ShippingMethod.cs
@@ -4,7 +4,7 @@
 	{
 		public string Name { get; set; }
 		public string Id { get; set; }
-		public bool RequireShippingAddress { get; set; }
+		public bool? RequireShippingAddress { get; set; }
 		public AlternateAddress AlternateAddress { get; set; }
 		public string CarrierCode { get; set; }
 	}
