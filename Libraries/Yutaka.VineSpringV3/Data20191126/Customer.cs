108434b2304836b295268ffb0c42e83aa3810742
Yutaka Blizman <blizman2017@gmail.com>
2019-12-02 15:41:56 -0800

    Changed DateTime CustomerSince to DateTime?. Re-ordered fields

--- Libraries/Yutaka.VineSpringV3/Data20191126/Customer.cs
+++ Libraries/Yutaka.VineSpringV3/Data20191126/Customer.cs
@@ -4,12 +4,12 @@ namespace Yutaka.VineSpringV3.Data20191126
 {
 	public class Customer
 	{
+		public DateTime CreatedOn { get; set; }
 		public string Id { get; set; }
 		public string AccountId { get; set; }
 		public string AltEmail { get; set; }
 		public string Company { get; set; }
-		public DateTime CreatedOn { get; set; }
-		public DateTime CustomerSince { get; set; }
+		public DateTime? CustomerSince { get; set; }
 		public BillingAddress BillingAddress { get; set; }
 		public DefaultAddress DefaultAddress { get; set; }
 		public DateTime? Dob { get; set; }
@@ -17,11 +17,11 @@ namespace Yutaka.VineSpringV3.Data20191126
 		public string FullName { get; set; }
 		public bool? IsTaxExempt { get; set; }
 		public string Password { get; set; }
-		public string UpdatedBy { get; set; }
-		public DateTime UpdatedOn { get; set; }
 		public Name Name { get; set; }
 		public string Phone { get; set; }
 		public string Source { get; set; }
+		public string UpdatedBy { get; set; }
+		public DateTime UpdatedOn { get; set; }
 	}
 
 	public class Name
