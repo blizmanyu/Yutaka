4aee7379b86139f19cf2dfbbd5cb655893b5f66b
Yutaka Blizman <blizman2017@gmail.com>
2019-12-02 17:48:29 -0800

    Changed some ints to decimals

--- Libraries/Yutaka.VineSpringV3/Data20191126/Item.cs
+++ Libraries/Yutaka.VineSpringV3/Data20191126/Item.cs
@@ -2,14 +2,14 @@
 {
 	public class Item
 	{
-		public string ShipCompliantBrandKey { get; set; }
-		public int Total { get; set; }
-		public int Quantity { get; set; }
 		public string ProductId { get; set; }
-		public int Price { get; set; }
+		public string ShipCompliantBrandKey { get; set; }
+		public decimal Total { get; set; }
+		public int Quantity { get; set; }
+		public decimal Price { get; set; }
 		public decimal Subtotal { get; set; }
 		public string Name { get; set; }
-		public decimal Discount { get; set; }
+		public decimal? Discount { get; set; }
 		public string ShipCompliantProductKey { get; set; }
 		public string Sku { get; set; }
 	}
