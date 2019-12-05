c9e76f24b435b579df33df476caebbd2ee536b7a
Yutaka Blizman <blizman2017@gmail.com>
2019-12-04 17:54:21 -0800

    Made programs readonly. Added check to not run on New Years Eve. General refactoring

--- Programs/RestartComputer/Program.cs
+++ Programs/RestartComputer/Program.cs
@@ -1,35 +1,48 @@
 ﻿using System;
+using System.Threading;
 using Yutaka.Diagnostics;
 
 namespace RestartComputer
 {
 	class Program
 	{
-		private static string[] programs = new string[] { "Dynamics", "OUTLOOK", "Skype", };
+		private static readonly string[] programs = new string[] { "Dynamics", "OUTLOOK", "Skype", };
 
 		static void Main(string[] args)
 		{
+			if (DateTime.Now.Month == 12 && DateTime.Now.Day == 31)
+				return; // don't run on New Years Eve //
+
 			if (ProcessUtil.GetUpTime() > ProcessUtil.FIVE_DAYS_IN_SECONDS) {
 				var killCount = 0;
 				var closeCount = 0;
 
 				foreach (var program in programs) {
-					if (ProcessUtil.CloseProgram(program) > 0) {
-						Console.Write("\nClosed {0}", program);
-						closeCount++;
+					try {
+						closeCount += ProcessUtil.CloseProgram(program);
 					}
+
+					catch (Exception) { }
 				}
 
-				ProcessUtil.RefreshTrayArea();
+				if (closeCount > 0) {
+					ProcessUtil.RefreshTrayArea();
+					Thread.Sleep(ProcessUtil.DEFAULT_SLEEP_TIME);
+				}
 
 				foreach (var program in programs) {
-					if (ProcessUtil.KillProcess(program) > 0) {
-						Console.Write("\nKilled {0}", program);
-						killCount++;
+					try {
+						killCount += ProcessUtil.KillProcess(program);
 					}
+
+					catch (Exception) { }
 				}
 
-				ProcessUtil.RefreshTrayArea();
+				if (killCount > 0) {
+					ProcessUtil.RefreshTrayArea();
+					Thread.Sleep(ProcessUtil.DEFAULT_SLEEP_TIME);
+				}
+
 				Console.Write("\nClosed {0} programs and killed {1}", closeCount, killCount);
 				ProcessUtil.RestartComputer();
 			}
