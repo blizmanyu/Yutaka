ba26546247ab1c1f27842a199e5f1af6070dd9b6
Yutaka Blizman <blizman2017@gmail.com>
2019-12-06 17:22:37 -0800

    Minor - just re-ordered fields

--- Libraries/Yutaka.VineSpring/Data20191126/Order.cs
+++ Libraries/Yutaka.VineSpring/Data20191126/Order.cs
@@ -6,30 +6,30 @@ namespace Yutaka.VineSpring.Data20191126
 	{
 		public DateTime CreatedOn { get; set; }
 		public string Id { get; set; }
-		public string Status { get; set; }
-		public decimal? Tax { get; set; }
-		public string SalesRep { get; set; }
-		public string FulfillmentHouse { get; set; }
-		public decimal? Shipping { get; set; }
 		public bool? IsCommitted { get; set; }
-		public DateTime? ShipDate { get; set; }
 		public bool? IsCompliant { get; set; }
-		public string AccountId { get; set; }
-		public string ShipCompliantStatus { get; set; }
-		public decimal? Total { get; set; }
-		public string Source { get; set; }
-		public string FullName { get; set; }
-		public decimal? FreightTax { get; set; }
-		public string OrderNumber { get; set; }
-		public decimal? Subtotal { get; set; }
+		public DateTime? ShipDate { get; set; }
 		public decimal? CalculatedShipping { get; set; }
-		public string CustomerId { get; set; }
-		public string Type { get; set; }
-		public string FulfillmentDetail { get; set; }
-		public string TrackingNumber { get; set; }
-		public string[] Tags { get; set; }
+		public decimal? FreightTax { get; set; }
+		public decimal? Shipping { get; set; }
+		public decimal? Subtotal { get; set; }
+		public decimal? Tax { get; set; }
+		public decimal? Total { get; set; }
+		public string AccountId { get; set; }
 		public string CampaignId { get; set; }
 		public string ComplianceDetail { get; set; }
+		public string CustomerId { get; set; }
+		public string FulfillmentDetail { get; set; }
+		public string FulfillmentHouse { get; set; }
+		public string FullName { get; set; }
+		public string OrderNumber { get; set; }
+		public string SalesRep { get; set; }
+		public string ShipCompliantStatus { get; set; }
+		public string Source { get; set; }
+		public string Status { get; set; }
+		public string TrackingNumber { get; set; }
+		public string Type { get; set; }
+		public string[] Tags { get; set; }
 		public Customer Customer { get; set; }
 		public Discount[] Discounts { get; set; }
 		public Item[] Items { get; set; }
