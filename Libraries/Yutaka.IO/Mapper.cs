31a38272001341ae6e0c20eedb74b532427188dc
Yutaka Blizman <blizman2017@gmail.com>
2019-12-18 17:41:00 -0800

    Now pulls only the class name instead of entire type. Now returns null on null input check

--- Libraries/Yutaka.IO/Mapper.cs
+++ Libraries/Yutaka.IO/Mapper.cs
@@ -9,14 +9,18 @@ namespace Yutaka.IO
 		public static string Map<TDest>(string sourceType)
 		{
 			var destType = typeof(TDest);
+			var destTypeName = destType.Name;
 			var sb = new StringBuilder();
 			sb.Append("\n");
-			sb.Append(String.Format("\n\t\tprivate static {0} Map({1} x)", destType, sourceType));
+			sb.Append(String.Format("\t\tprivate static {0} To{0}({1} x)", destTypeName, sourceType));
 			sb.Append("\n\t\t{");
-			sb.Append(String.Format("\n\t\t\treturn new {0} {{", destType));
+			sb.Append("\n\t\t\tif (x == null)");
+			sb.Append("\n\t\t\t\treturn null;");
+			sb.Append("\n");
+			sb.Append(String.Format("\n\t\t\treturn new {0} {{", destTypeName));
 
 			foreach (var p in destType.GetProperties().ToDictionary(p => p.Name, p => p))
-				sb.Append(String.Format("\n\t\t\t\t{0} = x.{0},", p.Key));
+				sb.Append(String.Format("\n\t\t\t\t{0} = x.{0} ?? null,", p.Key));
 
 			sb.Append("\n\t\t\t};");
 			sb.Append("\n\t\t}");
--- Programs/CodeGenerator/Program.cs
+++ Programs/CodeGenerator/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using Rcw.Sql.Hulk.Import.Data;
+using Rcw.Sql.Hulk.IntranetData.Data;
 using Yutaka.IO;
 
 namespace CodeGenerator
@@ -40,16 +41,16 @@ namespace CodeGenerator
 		private static void Process()
 		{
 			var sb = new StringBuilder();
-			sb.Append(Mapper.Map<Order>("VSData.Order"));
-			sb.Append(Mapper.Map<Order_Customer>("VSData.Order_Customer"));
-			sb.Append(Mapper.Map<Order_Customer_BillingAddress>("VSData.Order_Customer_BillingAddress"));
-			sb.Append(Mapper.Map<Order_Customer_Name>("VSData.Order_Customer_Name"));
-			sb.Append(Mapper.Map<Order_Discount>("VSData.Order_Discount"));
-			sb.Append(Mapper.Map<Order_Item>("VSData.Order_Item"));
-			sb.Append(Mapper.Map<Order_Note>("VSData.Order_Note"));
-			sb.Append(Mapper.Map<Order_ShippingAddress>("VSData.Order_ShippingAddress"));
-			sb.Append(Mapper.Map<Order_ShippingMethod>("VSData.Order_ShippingMethod"));
-			sb.Append(Mapper.Map<Order_ShippingMethod_AlternateAddress>("VSData.Order_ShippingMethod_AlternateAddress"));
+			sb.Append(Mapper.Map<InventoryAab>("InventoryAabView"));
+			//sb.Append(Mapper.Map<Order_Customer>("VSData.Order_Customer"));
+			//sb.Append(Mapper.Map<Order_Customer_BillingAddress>("VSData.Order_Customer_BillingAddress"));
+			//sb.Append(Mapper.Map<Order_Customer_Name>("VSData.Order_Customer_Name"));
+			//sb.Append(Mapper.Map<Order_Discount>("VSData.Order_Discount"));
+			//sb.Append(Mapper.Map<Order_Item>("VSData.Order_Item"));
+			//sb.Append(Mapper.Map<Order_Note>("VSData.Order_Note"));
+			//sb.Append(Mapper.Map<Order_ShippingAddress>("VSData.Order_ShippingAddress"));
+			//sb.Append(Mapper.Map<Order_ShippingMethod>("VSData.Order_ShippingMethod"));
+			//sb.Append(Mapper.Map<Order_ShippingMethod_AlternateAddress>("VSData.Order_ShippingMethod_AlternateAddress"));
 
 			var filename = String.Format("{0:yyyy MMdd HHmm ssff}.cs", DateTime.Now);
 			new FileUtil().Write(sb, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), filename));
