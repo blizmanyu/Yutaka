14dc50d857a4e48b1420746a1f2046c57384f61c
Yutaka Blizman <blizman2017@gmail.com>
2020-01-09 12:21:38 -0800

    New method public static string ConvertToRelativeTimeString(DateTime dt, TimeZoneInfo sourceTimeZone, TimeZoneInfo destinationTimeZone)

--- Libraries/Yutaka.DateTime/DateTimeUtil.cs
+++ Libraries/Yutaka.DateTime/DateTimeUtil.cs
@@ -33,7 +33,7 @@ namespace Yutaka
 		/// <summary>
 		/// Converts the date and time to current local date and time
 		/// </summary>
-		/// <param name="dt">The date and time to convert.</param>
+		/// <param name="dt">The dateTime to convert.</param>
 		/// <param name="sourceTimeZone">The time zone of dateTime.</param>
 		/// <returns>A DateTime value that represents time that corresponds to the dateTime parameter in customer time zone.</returns>
 		public static DateTime ConvertToLocalTime(DateTime dt, TimeZoneInfo sourceTimeZone)
@@ -44,7 +44,7 @@ namespace Yutaka
 		/// <summary>
 		/// Converts the date and time to current local date and time
 		/// </summary>
-		/// <param name="dt">The date and time to convert.</param>
+		/// <param name="dt">The dateTime to convert.</param>
 		/// <param name="sourceTimeZone">The time zone of dateTime.</param>
 		/// <param name="destinationTimeZone">The time zone to convert dateTime to.</param>
 		/// <returns>A DateTime value that represents time that corresponds to the dateTime parameter in customer time zone.</returns>
@@ -80,7 +80,7 @@ namespace Yutaka
 		/// <summary>
 		/// Converts the date and time to Coordinated Universal Time (UTC)
 		/// </summary>
-		/// <param name="dt">The date and time to convert.</param>
+		/// <param name="dt">The dateTime to convert.</param>
 		/// <param name="sourceTimeZone">The time zone of dateTime.</param>
 		/// <returns>A DateTime value that represents the Coordinated Universal Time (UTC) that corresponds to the dateTime parameter. The DateTime value's Kind property is always set to DateTimeKind.Utc.</returns>
 		public static DateTime ConvertToUtcTime(DateTime dt, TimeZoneInfo sourceTimeZone)
@@ -99,7 +99,7 @@ namespace Yutaka
 		/// Converts the date and time to current local date and time
 		/// </summary>
 		/// <param name="dt">The date and time (respesents local system time or UTC time) to convert.</param>
-		/// <returns>A DateTime value that represents time that corresponds to the dateTime parameter in customer time zone.</returns>
+		/// <returns>A string that represents time that corresponds to the dateTime parameter in local time zone.</returns>
 		public static string ConvertToRelativeTimeString(DateTime dt)
 		{
 			return ConvertToRelativeTimeString(dt, dt.Kind);
@@ -110,7 +110,7 @@ namespace Yutaka
 		/// </summary>
 		/// <param name="dt">The date and time (respesents local system time or UTC time) to convert.</param>
 		/// <param name="sourceDateTimeKind">The source datetimekind</param>
-		/// <returns>A DateTime value that represents time that corresponds to the dateTime parameter in customer time zone.</returns>
+		/// <returns>A string that represents time that corresponds to the dateTime parameter in local time zone.</returns>
 		public static string ConvertToRelativeTimeString(DateTime dt, DateTimeKind sourceDateTimeKind)
 		{
 			dt = ConvertToLocalTime(dt, sourceDateTimeKind);
@@ -120,6 +120,29 @@ namespace Yutaka
 				return String.Format("Today, {0}", time);
 
 			if (dt.Year == DateTime.Now.Year) // This Year //
+				return String.Format("{0:MMM d}, {1}", dt, time);
+
+			return String.Format("{0:M/d/yy}, {1}", dt, time);
+		}
+
+		/// <summary>
+		/// Converts the date and time to current local date and time
+		/// </summary>
+		/// <param name="dt">The dateTime to convert.</param>
+		/// <param name="sourceTimeZone">The time zone of dateTime.</param>
+		/// <param name="destinationTimeZone">The time zone to convert dateTime to.</param>
+		/// <returns>A string that represents time that corresponds to the dateTime parameter in local time zone.</returns>
+		public static string ConvertToRelativeTimeString(DateTime dt, TimeZoneInfo sourceTimeZone, TimeZoneInfo destinationTimeZone)
+		{
+			dt = ConvertToLocalTime(dt, sourceTimeZone, destinationTimeZone);
+			var time = dt.ToString("h:mmtt").ToLower();
+			var todayAtDestination = ConvertToLocalTime(DateTime.Today, TimeZoneInfo.Local, destinationTimeZone);
+			var thisYearAtDestination = todayAtDestination.Year;
+
+			if (dt.Date == todayAtDestination)
+				return String.Format("Today, {0}", time);
+
+			if (dt.Year == thisYearAtDestination) // This Year //
 				return String.Format("{0:MMM d}, {1}", dt, time);
 
 			return String.Format("{0:M/d/yy}, {1}", dt, time);
