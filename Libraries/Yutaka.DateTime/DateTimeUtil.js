0fec7f8b8dd9795c8e3dd292aa86d6eb3bb61f1d
Yutaka Blizman <blizman2017@gmail.com>
2020-01-15 18:54:24 -0800

    New file DateTimeUtil.js

---
+++ Libraries/Yutaka.DateTime/DateTimeUtil.js
@@ -0,0 +1,24 @@
+﻿function GetBeginningOfQuarter() {
+	var now = new Date();
+	var quarter = Math.floor((now.getMonth() / 3));
+	return new Date(now.getFullYear(), quarter * 3, 1);
+}
+
+function GetEndOfQuarter() {
+	var beginningOfQuarter = GetBeginningOfQuarter();
+	return new Date(beginningOfQuarter.getFullYear(), beginningOfQuarter.getMonth() + 3, 0);
+}
+
+function AddMonths(x) {
+	var now = new Date();
+	now.setMonth(now.getMonth() + x);
+	return now;
+}
+
+function GetBeginningOfYear() {
+	return new Date(new Date().getFullYear(), 0, 1);
+}
+
+function GetEndOfYear() {
+	return new Date(new Date().getFullYear(), 11, 31);
+}
