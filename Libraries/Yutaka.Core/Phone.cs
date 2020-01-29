f6da0721b5a86858c7bebfe80c38621a8582f027
Yutaka Blizman <blizman2017@gmail.com>
2020-01-28 18:28:20 -0800

    Added fields

--- Libraries/Yutaka.Core/Phone.cs
+++ Libraries/Yutaka.Core/Phone.cs
@@ -8,5 +8,22 @@ namespace Yutaka
 {
 	public class Phone
 	{
+		public DateTime CreatedOnUtc;
+		public int CreatedById;
+		public int Id;
+		public int ContactId;
+
+		public bool IsPrimary;
+		public string Label;
+		public string Number;
+		public string Ext;
+
+		public int UpdatedById;
+		public DateTime UpdatedOnUtc;
+
+		//public Phone(string number = null, string ext = null, string label = null, bool isPrimary = null)
+		//{
+
+		//}
 	}
 }
