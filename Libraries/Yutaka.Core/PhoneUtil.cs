d6cee5e3c69a0002ae92510dd8984c752771c133
Yutaka Blizman <blizman2017@gmail.com>
2020-01-28 18:28:41 -0800

    New method public static string Minify(string phone)

--- Libraries/Yutaka.Core/PhoneUtil.cs
+++ Libraries/Yutaka.Core/PhoneUtil.cs
@@ -8,5 +8,22 @@ namespace Yutaka
 {
 	public static class PhoneUtil
 	{
+		/// <summary>
+		/// Removes all non-alphanumerics from a phone number except for '+'.
+		/// </summary>
+		/// <param name="phone"></param>
+		/// <returns></returns>
+		public static string Minify(string phone)
+		{
+			if (String.IsNullOrWhiteSpace(phone))
+				return "";
+
+			var minified = phone.Replace("`", "").Replace("~", "").Replace("!", "").Replace("@", "").Replace("$", "").Replace("%", "").Replace("^", "").Replace("&", "").Replace("*", "").Replace("(", "").Replace(")", "").Replace("_", "").Replace("-", "").Replace("=", "").Replace("{", "").Replace("[", "").Replace("}", "").Replace("]", "").Replace("|", "").Replace(@"\", "").Replace(":", "").Replace(";", "").Replace("\"", "").Replace("'", "").Replace("<", "").Replace(",", "").Replace(">", "").Replace(".", "").Replace("/", "").Replace(" ", "");
+
+			while (minified.Contains(" "))
+				minified = minified.Replace(" ", "");
+
+			return minified;
+		}
 	}
 }
