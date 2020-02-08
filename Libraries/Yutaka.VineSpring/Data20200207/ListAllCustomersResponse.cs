dbf57c0f4066a8053c95a97edba745ae410e2ec2
Yutaka Blizman <blizman2017@gmail.com>
2020-02-07 16:29:03 -0800

    Capitalized all fields. Made relevant fields nullable

--- Libraries/Yutaka.VineSpring/Data20200207/ListAllCustomersResponse.cs
+++ Libraries/Yutaka.VineSpring/Data20200207/ListAllCustomersResponse.cs
@@ -9,51 +9,51 @@ namespace Yutaka.VineSpring.Data20200207
 
 	public class Customer
 	{
-		public DateTime updatedOn { get; set; }
-		public string accountId { get; set; }
-		public bool isTaxExempt { get; set; }
-		public string updatedBy { get; set; }
-		public string source { get; set; }
-		public string fullName { get; set; }
-		public string email { get; set; }
-		public Name name { get; set; }
-		public string authToken { get; set; }
-		public DateTime customerSince { get; set; }
-		public string company { get; set; }
-		public DateTime createdOn { get; set; }
-		public string id { get; set; }
-		public float lifetimeValue { get; set; }
-		public string lastName { get; set; }
-		public string firstName { get; set; }
-		public Defaultaddress defaultAddress { get; set; }
-		public DateTime dob { get; set; }
-		public string phone { get; set; }
-		public string merchantId { get; set; }
+		public DateTime? UpdatedOn { get; set; }
+		public string AccountId { get; set; }
+		public bool? IsTaxExempt { get; set; }
+		public string UpdatedBy { get; set; }
+		public string Source { get; set; }
+		public string FullName { get; set; }
+		public string Email { get; set; }
+		public Name Name { get; set; }
+		public string AuthToken { get; set; }
+		public DateTime? CustomerSince { get; set; }
+		public string Company { get; set; }
+		public DateTime? CreatedOn { get; set; }
+		public string Id { get; set; }
+		public decimal? LifetimeValue { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public DefaultAddress DefaultAddress { get; set; }
+		public DateTime? Dob { get; set; }
+		public string Phone { get; set; }
+		public string MerchantId { get; set; }
 	}
 
 	public class Name
 	{
-		public string last { get; set; }
-		public string first { get; set; }
-		public string middle { get; set; }
+		public string Last { get; set; }
+		public string First { get; set; }
+		public string Middle { get; set; }
 	}
 
-	public class Defaultaddress
+	public class DefaultAddress
 	{
-		public string updatedBy { get; set; }
-		public string city { get; set; }
-		public string postalCode { get; set; }
-		public string name { get; set; }
-		public string customerId { get; set; }
-		public DateTime updatedOn { get; set; }
-		public string id { get; set; }
-		public string state { get; set; }
-		public DateTime createdOn { get; set; }
-		public string line1 { get; set; }
-		public bool isInternational { get; set; }
-		public string phone { get; set; }
-		public string organization { get; set; }
-		public string country { get; set; }
-		public string line2 { get; set; }
+		public string UpdatedBy { get; set; }
+		public string City { get; set; }
+		public string PostalCode { get; set; }
+		public string Name { get; set; }
+		public string CustomerId { get; set; }
+		public DateTime? UpdatedOn { get; set; }
+		public string Id { get; set; }
+		public string State { get; set; }
+		public DateTime? CreatedOn { get; set; }
+		public string Line1 { get; set; }
+		public bool? IsInternational { get; set; }
+		public string Phone { get; set; }
+		public string Organization { get; set; }
+		public string Country { get; set; }
+		public string Line2 { get; set; }
 	}
 }
