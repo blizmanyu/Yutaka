b8a349b1f6c3b7148e03383bc78c533529c28af6
Yutaka Blizman <blizman2017@gmail.com>
2020-02-07 16:46:03 -0800

    Reordered fields

--- Libraries/Yutaka.VineSpring/Data20200207/ListAllCustomersResponse.cs
+++ Libraries/Yutaka.VineSpring/Data20200207/ListAllCustomersResponse.cs
@@ -9,51 +9,51 @@ namespace Yutaka.VineSpring.Data20200207
 
 	public class Customer
 	{
-		public DateTime? UpdatedOn { get; set; }
-		public string AccountId { get; set; }
-		public bool? IsTaxExempt { get; set; }
-		public string UpdatedBy { get; set; }
-		public string Source { get; set; }
-		public string FullName { get; set; }
-		public string Email { get; set; }
-		public Name Name { get; set; }
-		public string AuthToken { get; set; }
-		public DateTime? CustomerSince { get; set; }
-		public string Company { get; set; }
 		public DateTime? CreatedOn { get; set; }
 		public string Id { get; set; }
-		public decimal? LifetimeValue { get; set; }
-		public string LastName { get; set; }
-		public string FirstName { get; set; }
-		public DefaultAddress DefaultAddress { get; set; }
+		public bool? IsTaxExempt { get; set; }
+		public DateTime? CustomerSince { get; set; }
 		public DateTime? Dob { get; set; }
-		public string Phone { get; set; }
+		public decimal? LifetimeValue { get; set; }
+		public DefaultAddress DefaultAddress { get; set; }
+		public Name Name { get; set; }
+		public string AccountId { get; set; }
+		public string AuthToken { get; set; }
+		public string Company { get; set; }
+		public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string FullName { get; set; }
 		public string MerchantId { get; set; }
+		public string Phone { get; set; }
+		public string Source { get; set; }
+		public string UpdatedBy { get; set; }
+		public DateTime? UpdatedOn { get; set; }
 	}
 
 	public class Name
 	{
-		public string Last { get; set; }
 		public string First { get; set; }
 		public string Middle { get; set; }
+		public string Last { get; set; }
 	}
 
 	public class DefaultAddress
 	{
-		public string UpdatedBy { get; set; }
-		public string City { get; set; }
-		public string PostalCode { get; set; }
-		public string Name { get; set; }
-		public string CustomerId { get; set; }
-		public DateTime? UpdatedOn { get; set; }
-		public string Id { get; set; }
-		public string State { get; set; }
 		public DateTime? CreatedOn { get; set; }
-		public string Line1 { get; set; }
+		public string Id { get; set; }
 		public bool? IsInternational { get; set; }
-		public string Phone { get; set; }
+		public string Name { get; set; }
 		public string Organization { get; set; }
-		public string Country { get; set; }
+		public string Line1 { get; set; }
 		public string Line2 { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string PostalCode { get; set; }
+		public string Country { get; set; }
+		public string Phone { get; set; }
+		public string CustomerId { get; set; }
+		public string UpdatedBy { get; set; }
+		public DateTime? UpdatedOn { get; set; }
 	}
 }
