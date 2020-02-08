a2c9af712be7b156e14ca33574f2572e3941f5e8
Yutaka Blizman <blizman2017@gmail.com>
2020-02-07 17:22:12 -0800

    CustomerDefaultAddress now inherits Address

--- Libraries/Yutaka.VineSpring/Data20200207/ListAllCustomersResponse.cs
+++ Libraries/Yutaka.VineSpring/Data20200207/ListAllCustomersResponse.cs
@@ -33,22 +33,5 @@ namespace Yutaka.VineSpring.Data20200207
 		public string Last { get; set; }
 	}
 
-	public class CustomerDefaultAddress
-	{
-		public DateTime? CreatedOn { get; set; }
-		public string Id { get; set; }
-		public bool? IsInternational { get; set; }
-		public string Name { get; set; }
-		public string Organization { get; set; }
-		public string Line1 { get; set; }
-		public string Line2 { get; set; }
-		public string City { get; set; }
-		public string State { get; set; }
-		public string PostalCode { get; set; }
-		public string Country { get; set; }
-		public string Phone { get; set; }
-		public string CustomerId { get; set; }
-		public string UpdatedBy { get; set; }
-		public DateTime? UpdatedOn { get; set; }
-	}
+	public class CustomerDefaultAddress : Address { }
 }
