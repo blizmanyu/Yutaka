4d4699d975ae21d78868e53e070c446cdbfbce71
blizmanyu <blizman2017@gmail.com>
2020-03-05 22:53:16 -0800

    new public class SqlTable

---
+++ Programs/CodeGenerator/Data/SqlTable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CodeGenerator.Data
+{
+	public class SqlTable
+	{
+		public string Name;
+		public string Database;
+		public string Schema;
+		public List<SqlTableColumn> Columns;
+
+		public SqlTable()
+		{
+			Columns = new List<SqlTableColumn>();
+		}
+	}
+}
