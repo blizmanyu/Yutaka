985bf9e0809e7acd97d6a80f167cb020bad34b39
blizmanyu <blizman2017@gmail.com>
2020-03-17 00:27:53 -0700

    Debug() - made Size format "n0"

--- Libraries/Yutaka.IO2/YuFile.cs
+++ Libraries/Yutaka.IO2/YuFile.cs
@@ -260,7 +260,7 @@ namespace Yutaka.IO2
 			Console.Write("\n   ParentFolder: {0}", ParentFolder);
 			Console.Write("\n      NewFolder: {0}", NewFolder);
 			Console.Write("\n           Root: {0}", Root);
-			Console.Write("\n           Size: {0}", Size);
+			Console.Write("\n           Size: {0:n0}", Size);
 			Console.Write("\n");
 		}
 
