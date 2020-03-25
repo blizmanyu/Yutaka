c554a8ee7476c0d4ddcc52f9bae5bdc6d54e6ae4
blizmanyu <blizman2017@gmail.com>
2020-03-25 01:57:04 -0700

    Constructor - better input check. Added Summary

--- Libraries/Yutaka.Net/GmailSmtpClient.cs
+++ Libraries/Yutaka.Net/GmailSmtpClient.cs
@@ -6,12 +6,26 @@ namespace Yutaka.Net
 {
 	public class GmailSmtpClient : SmtpClient
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GmailSmtpClient"/> class that sends email by using Gmail's SMTP server and port.
+		/// </summary>
+		/// <param name="username">The user name associated with the credentials.</param>
+		/// <param name="password">The password for the user name associated with the credentials.</param>
 		public GmailSmtpClient(string username, string password)
 		{
+			#region Input Check
+			var log = "";
+
 			if (String.IsNullOrWhiteSpace(username))
-				throw new Exception(String.Format("<username> is required. Exception thrown in GmailSmtpClient Constructor.{0}", Environment.NewLine));
+				log = String.Format("{0}<username> is required.{1}", log, Environment.NewLine);
 			if (String.IsNullOrWhiteSpace(password))
-				throw new Exception(String.Format("<password> is required. Exception thrown in GmailSmtpClient Constructor.{0}", Environment.NewLine));
+				log = String.Format("{0}<password> is required.{1}", log, Environment.NewLine);
+
+			if (!String.IsNullOrWhiteSpace(log)) {
+				log = String.Format("{0}Exception thrown in Constructor GmailSmtpClient(string username, string password).{1}{1}", log, Environment.NewLine);
+				throw new Exception(log);
+			}
+			#endregion Input Check
 
 			UseDefaultCredentials = false; // always set this BEFORE setting Credentials //
 			Credentials = new NetworkCredential(username, password);
