88905d74b03f1248bddf619f5c5d7fc94d71931f
blizmanyu <blizman2017@gmail.com>
2020-04-08 04:16:37 -0700

    Created all tests

--- Tests/SpeedTester/Program.cs
+++ Tests/SpeedTester/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 
@@ -15,6 +17,7 @@ namespace SpeedTester
 		private static readonly DateTime StartTime = DateTime.Now;
 		private static readonly HashSet<string> imageExtensionsHashSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".ai", ".bmp", ".eps", ".exif", ".gif", ".ico", ".jpeg", ".jpg", ".nef", ".png", ".psd", ".svg", ".tiff", ".webp", };
 		private static readonly Regex imageExtensionsRegex = new Regex(".ai|.bmp|.eps|.exif|.gif|.ico|.jpeg|.jpg|.nef|.png|.psd|.svg|.tiff|.webp", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled);
+		private static readonly string path = @"asdfasdf\";
 		private static readonly string[] imageExtensionsArray = { ".ai", ".bmp", ".eps", ".exif", ".gif", ".ico", ".jpeg", ".jpg", ".nef", ".png", ".psd", ".svg", ".tiff", ".webp", };
 		private static Stopwatch sw = new Stopwatch();
 		#endregion Fields
@@ -30,6 +33,21 @@ namespace SpeedTester
 			Test1();
 			Test2();
 			Test3();
+			Test4();
+			Test5();
+			Test6();
+			Test1();
+			Test2();
+			Test3();
+			Test4();
+			Test5();
+			Test6();
+			Test1();
+			Test2();
+			Test3();
+			Test4();
+			Test5();
+			Test6();
 			EndProgram();
 		}
 
@@ -37,39 +55,96 @@ namespace SpeedTester
 		{
 			sw.Reset();
 			sw.Start();
+			var count = 0;
+			var files = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Where(x => imageExtensionsHashSet.Contains(Path.GetExtension(x)));
 
-			for (var i = 0; i < iterations; i++) {
-				;
-			}
+			for (int i = 0; i < files.Count(); i++)
+				count++;
 
 			sw.Stop();
-			Console.Write("Test1 took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
+			Console.Write("\n");
+			Console.Write("Count: {0}\n", count);
+			Console.Write("Directory.EnumerateFiles() HashSet took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
 		}
 
 		private static void Test2()
 		{
 			sw.Reset();
 			sw.Start();
+			var count = 0;
+			var files = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Where(x => imageExtensionsRegex.IsMatch(Path.GetExtension(x)));
 
-			for (var i = 0; i < iterations; i++) {
-				;
-			}
+			for (int i = 0; i < files.Count(); i++)
+				count++;
 
 			sw.Stop();
-			Console.Write("Test2 took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
+			Console.Write("\n");
+			Console.Write("Count: {0}\n", count);
+			Console.Write("Directory.EnumerateFiles() Regex took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
 		}
 
 		private static void Test3()
 		{
 			sw.Reset();
 			sw.Start();
+			var count = 0;
+			var files = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Where(x => imageExtensionsArray.Contains(Path.GetExtension(x), StringComparer.OrdinalIgnoreCase));
 
-			for (var i = 0; i < iterations; i++) {
-				;
-			}
+			for (int i = 0; i < files.Count(); i++)
+				count++;
 
 			sw.Stop();
-			Console.Write("Test3 took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
+			Console.Write("\n");
+			Console.Write("Count: {0}\n", count);
+			Console.Write("Directory.EnumerateFiles() array took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
+		}
+
+		private static void Test4()
+		{
+			sw.Reset();
+			sw.Start();
+			var count = 0;
+			var files = new DirectoryInfo(path).EnumerateFiles("*", SearchOption.AllDirectories).Where(x => imageExtensionsHashSet.Contains(x.Extension));
+
+			for (int i = 0; i < files.Count(); i++)
+				count++;
+
+			sw.Stop();
+			Console.Write("\n");
+			Console.Write("Count: {0}\n", count);
+			Console.Write("DirectoryInfo.EnumerateFiles() HashSet took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
+		}
+
+		private static void Test5()
+		{
+			sw.Reset();
+			sw.Start();
+			var count = 0;
+			var files = new DirectoryInfo(path).EnumerateFiles("*", SearchOption.AllDirectories).Where(x => imageExtensionsRegex.IsMatch(x.Extension));
+
+			for (int i = 0; i < files.Count(); i++)
+				count++;
+
+			sw.Stop();
+			Console.Write("\n");
+			Console.Write("Count: {0}\n", count);
+			Console.Write("DirectoryInfo.EnumerateFiles() Regex took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
+		}
+
+		private static void Test6()
+		{
+			sw.Reset();
+			sw.Start();
+			var count = 0;
+			var files = new DirectoryInfo(path).EnumerateFiles("*", SearchOption.AllDirectories).Where(x => imageExtensionsArray.Contains(x.Extension, StringComparer.OrdinalIgnoreCase));
+
+			for (int i = 0; i < files.Count(); i++)
+				count++;
+
+			sw.Stop();
+			Console.Write("\n");
+			Console.Write("Count: {0}\n", count);
+			Console.Write("DirectoryInfo.EnumerateFiles() array took {0}\n", sw.Elapsed.ToString(@"hh\:mm\:ss\.fff"));
 		}
 
 		#region StartProgram & EndProgram
