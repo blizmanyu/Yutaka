d737842f9f2e0b31183bd45e571454d63bb98798
blizmanyu <blizman2017@gmail.com>
2020-04-15 23:41:35 -0700

    GetColumnsInformation() - added try-catch block with logging

--- Libraries/Yutaka.Tsql/TsqlUtil.cs
+++ Libraries/Yutaka.Tsql/TsqlUtil.cs
@@ -56,40 +56,56 @@ namespace Yutaka.Data
 					where = String.Format("{0}   AND [TABLE_NAME] = '{1}'", where, table);
 			}
 
-			using (var conn = new SqlConnection(connectionString)) {
-				using (var cmd = new SqlCommand(String.Format("{0}{1}", query, where), conn)) {
-					cmd.CommandType = CommandType.Text;
-					conn.Open();
-					using (var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection)) {
-						while (reader.Read()) {
-							list.Add(new Column {
-								TableCatalog = reader["TABLE_CATALOG"] is DBNull ? "" : reader["TABLE_CATALOG"].ToString(),
-								TableSchema = reader["TABLE_SCHEMA"] is DBNull ? "" : reader["TABLE_SCHEMA"].ToString(),
-								TableName = reader["TABLE_NAME"] is DBNull ? "" : reader["TABLE_NAME"].ToString(),
-								ColumnName = reader["COLUMN_NAME"] is DBNull ? "" : reader["COLUMN_NAME"].ToString(),
-								OrdinalPosition = reader["ORDINAL_POSITION"] is DBNull ? -1 : (int) reader["ORDINAL_POSITION"],
-								ColumnDefault = reader["COLUMN_DEFAULT"]?.ToString(),
-								IsNullable = reader["IS_NULLABLE"] is DBNull ? true : reader["IS_NULLABLE"].Equals("YES") ? true : false,
-								DataType = reader["DATA_TYPE"] is DBNull ? "" : reader["DATA_TYPE"].ToString(),
-								CharacterMaximumLength = reader["CHARACTER_MAXIMUM_LENGTH"] is DBNull ? -1 : (int) reader["CHARACTER_MAXIMUM_LENGTH"],
-								CharacterOctetLength = reader["CHARACTER_OCTET_LENGTH"] is DBNull ? -1 : (int) reader["CHARACTER_OCTET_LENGTH"],
-								NumericPrecision = reader["NUMERIC_PRECISION"] is DBNull ? -1 : int.Parse(reader["NUMERIC_PRECISION"].ToString()),
-								NumericPrecisionRadix = reader["NUMERIC_PRECISION_RADIX"] is DBNull ? -1 : int.Parse(reader["NUMERIC_PRECISION_RADIX"].ToString()),
-								NumericScale = reader["NUMERIC_SCALE"] is DBNull ? -1 : (int) reader["NUMERIC_SCALE"],
-								DatetimePrecision = reader["DATETIME_PRECISION"] is DBNull ? -1 : int.Parse(reader["DATETIME_PRECISION"].ToString()),
-								CharacterSetCatalog = reader["CHARACTER_SET_CATALOG"]?.ToString(),
-								CharacterSetSchema = reader["CHARACTER_SET_SCHEMA"]?.ToString(),
-								CharacterSetName = reader["CHARACTER_SET_NAME"]?.ToString(),
-								CollationCatalog = reader["COLLATION_CATALOG"]?.ToString(),
-								CollationSchema = reader["COLLATION_SCHEMA"]?.ToString(),
-								CollationName = reader["COLLATION_NAME"]?.ToString(),
-								DomainCatalog = reader["DOMAIN_CATALOG"]?.ToString(),
-								DomainSchema = reader["DOMAIN_SCHEMA"]?.ToString(),
-								DomainName = reader["DOMAIN_NAME"]?.ToString(),
-							});
+			try {
+				using (var conn = new SqlConnection(connectionString)) {
+					using (var cmd = new SqlCommand(String.Format("{0}{1}", query, where), conn)) {
+						cmd.CommandType = CommandType.Text;
+						conn.Open();
+						using (var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection)) {
+							while (reader.Read()) {
+								list.Add(new Column {
+									TableCatalog = reader["TABLE_CATALOG"] is DBNull ? "" : reader["TABLE_CATALOG"].ToString(),
+									TableSchema = reader["TABLE_SCHEMA"] is DBNull ? "" : reader["TABLE_SCHEMA"].ToString(),
+									TableName = reader["TABLE_NAME"] is DBNull ? "" : reader["TABLE_NAME"].ToString(),
+									ColumnName = reader["COLUMN_NAME"] is DBNull ? "" : reader["COLUMN_NAME"].ToString(),
+									OrdinalPosition = reader["ORDINAL_POSITION"] is DBNull ? -1 : (int) reader["ORDINAL_POSITION"],
+									ColumnDefault = reader["COLUMN_DEFAULT"]?.ToString(),
+									IsNullable = reader["IS_NULLABLE"] is DBNull ? true : reader["IS_NULLABLE"].Equals("YES") ? true : false,
+									DataType = reader["DATA_TYPE"] is DBNull ? "" : reader["DATA_TYPE"].ToString(),
+									CharacterMaximumLength = reader["CHARACTER_MAXIMUM_LENGTH"] is DBNull ? -1 : (int) reader["CHARACTER_MAXIMUM_LENGTH"],
+									CharacterOctetLength = reader["CHARACTER_OCTET_LENGTH"] is DBNull ? -1 : (int) reader["CHARACTER_OCTET_LENGTH"],
+									NumericPrecision = reader["NUMERIC_PRECISION"] is DBNull ? -1 : int.Parse(reader["NUMERIC_PRECISION"].ToString()),
+									NumericPrecisionRadix = reader["NUMERIC_PRECISION_RADIX"] is DBNull ? -1 : int.Parse(reader["NUMERIC_PRECISION_RADIX"].ToString()),
+									NumericScale = reader["NUMERIC_SCALE"] is DBNull ? -1 : (int) reader["NUMERIC_SCALE"],
+									DatetimePrecision = reader["DATETIME_PRECISION"] is DBNull ? -1 : int.Parse(reader["DATETIME_PRECISION"].ToString()),
+									CharacterSetCatalog = reader["CHARACTER_SET_CATALOG"]?.ToString(),
+									CharacterSetSchema = reader["CHARACTER_SET_SCHEMA"]?.ToString(),
+									CharacterSetName = reader["CHARACTER_SET_NAME"]?.ToString(),
+									CollationCatalog = reader["COLLATION_CATALOG"]?.ToString(),
+									CollationSchema = reader["COLLATION_SCHEMA"]?.ToString(),
+									CollationName = reader["COLLATION_NAME"]?.ToString(),
+									DomainCatalog = reader["DOMAIN_CATALOG"]?.ToString(),
+									DomainSchema = reader["DOMAIN_SCHEMA"]?.ToString(),
+									DomainName = reader["DOMAIN_NAME"]?.ToString(),
+								});
+							}
 						}
 					}
 				}
+			}
+
+			catch (Exception ex) {
+				#region Log
+				string log;
+
+				if (ex.InnerException == null)
+					log = String.Format("{0}{2}Exception thrown in TsqlUtil.GetColumnsInformation(string connectionString, string database='{3}', string schema='{4}', string table='{5}').{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, database, schema, table);
+				else
+					log = String.Format("{0}{2}Exception thrown in INNER EXCEPTION of TsqlUtil.GetColumnsInformation(string connectionString, string database='{3}', string schema='{4}', string table='{5}').{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, database, schema, table);
+
+				Console.Write("\n{0}", log);
+				throw new Exception(log);
+				#endregion Log
 			}
 
 			return list;
