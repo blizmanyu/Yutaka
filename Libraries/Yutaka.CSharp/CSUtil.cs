1143b6f27de9448ea82161ce06db56fe9aa668da
blizmanyu <blizman2017@gmail.com>
2020-04-19 22:08:15 -0700

    GenerateGetById() - changed parameter to IList<Column> columns

--- Libraries/Yutaka.CSharp/CSUtil.cs
+++ Libraries/Yutaka.CSharp/CSUtil.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Yutaka.Data;
 
 namespace Yutaka.Text
 {
@@ -9,17 +11,40 @@ namespace Yutaka.Text
 		/// </summary>
 		/// <param name="table">The name of the table.</param>
 		/// <returns></returns>
-		public static string GenerateGetById(string table)
+		public static string GenerateGetById(IList<Column> columns)
 		{
+			var table = "";
+			var idCol = "";
+			var dataType = "";
+
+			foreach (var col in columns) {
+				if (col.ColumnName.Equals("Id") || col.ColumnName.Equals("UniqueId")) {
+					table = col.TableName;
+					idCol = col.ColumnName;
+					dataType = col.DataType;
+					break;
+				}
+			}
+
 			var alias = table.Replace("_", "").Substring(0, 1).ToLower();
 
+			if (dataType.Equals("int")) {
+				return String.Format(
+					"\t\tpublic {0} Get{0}ById(int id){3}" +
+					"\t\t{{{3}" +
+					"\t\t\treturn (from {1} in dbcontext.{0}s{3}" +
+					"\t\t\t\t\twhere id == {1}.{2}{3}" +
+					"\t\t\t\t\tselect {1}).FirstOrDefault();{3}" +
+					"\t\t}}{3}{3}", table, alias, idCol, Environment.NewLine);
+			}
+
 			return String.Format(
-				"\t\tpublic {0} Get{0}ById(int id){2}" +
-				"\t\t{{{2}" +
-				"\t\t\treturn (from {1} in dbcontext.{0}s{2}" +
-				"\t\t\t\t\twhere id == {1}.Id{2}" +
-				"\t\t\t\t\tselect {1}).FirstOrDefault();{2}" +
-				"\t\t}}{2}{2}", table, alias, Environment.NewLine);
+				"\t\tpublic {0} Get{0}ById(string id){3}" +
+				"\t\t{{{3}" +
+				"\t\t\treturn (from {1} in dbcontext.{0}s{3}" +
+				"\t\t\t\t\twhere id.Equals({1}.{2}){3}" +
+				"\t\t\t\t\tselect {1}).FirstOrDefault();{3}" +
+				"\t\t}}{3}{3}", table, alias, idCol, Environment.NewLine);
 		}
 	}
 }
