7d35754135856f4d8e53ecb4f3228b76ebc5565a
Yutaka Blizman <blizman2017@gmail.com>
2020-06-02 16:21:27 -0700

    New method protected string DeleteEvent(string eventId, string calendarId)

--- Libraries/Yutaka.Services/GoogleCalendarService.cs
+++ Libraries/Yutaka.Services/GoogleCalendarService.cs
@@ -83,6 +83,19 @@ namespace Yutaka.Google.Calendar
 		}
 
 		/// <summary>
+		/// Deletes an event.
+		/// </summary>
+		/// <param name="eventId">Event identifier.</param>
+		/// <param name="calendarId">Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to
+		/// access the primary calendar of the currently logged in user, use the "primary" keyword, or, the full email address if
+		/// you're using a service account.</param>
+		/// <returns></returns>
+		protected string DeleteEvent(string eventId, string calendarId)
+		{
+			return _service.Events.Delete(eventId, calendarId).Execute();
+		}
+
+		/// <summary>
 		/// Creates an event.
 		/// </summary>
 		/// <param name="ev">The body of the request.</param>
