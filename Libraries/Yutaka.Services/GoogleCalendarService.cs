af44fd6d2f082bad8a09e23387cd8970f9ce5838
Yutaka Blizman <blizman2017@gmail.com>
2020-06-05 17:50:41 -0700

    Removed DeleteEvent() since TryDeleteEvent() now executes Delete directly

--- Libraries/Yutaka.Services/GoogleCalendarService.cs
+++ Libraries/Yutaka.Services/GoogleCalendarService.cs
@@ -87,19 +87,6 @@ namespace Yutaka.Google.Calendar
 		}
 
 		/// <summary>
-		/// Deletes an event.
-		/// </summary>
-		/// <param name="eventId">Event identifier.</param>
-		/// <param name="calendarId">Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to
-		/// access the primary calendar of the currently logged in user, use the "primary" keyword, or, the full email address if
-		/// you're using a service account.</param>
-		/// <returns></returns>
-		protected string DeleteEvent(string eventId, string calendarId)
-		{
-			return _service.Events.Delete(calendarId, eventId).Execute();
-		}
-
-		/// <summary>
 		/// Gets an event.
 		/// </summary>
 		/// <param name="eventId">Event identifier.</param>
