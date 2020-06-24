8f272a0cb1b23623a5929bb0f5f5470dc706548a
Yutaka Blizman <blizman2017@gmail.com>
2020-06-24 14:15:53 -0700

    New field public string Body. New fields private static readonly string Tab & Space

--- Libraries/Yutaka.Core/CSharp/Method.cs
+++ Libraries/Yutaka.Core/CSharp/Method.cs
@@ -8,11 +8,14 @@ namespace Yutaka.Core.CSharp
 {
 	public class Method
 	{
+		private static readonly string Space = " ";
+		private static readonly string Tab = "\t";
 		public string AccessLevel;
 		public string Modifier;
 		public string ReturnType;
 		public string Name;
 		public string[] Parameters;
+		public string Body;
 
 		public Method(string accessLevel = null, string modifier = null, string returnType = null, string name = null, string[] parameters = null)
 		{
