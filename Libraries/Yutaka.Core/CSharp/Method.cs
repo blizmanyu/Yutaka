c77b1b8caef59df5f6847943e929d7954c2b1a83
Yutaka Blizman <blizman2017@gmail.com>
2020-06-24 12:13:06 -0700

    Added fields and Constructor

--- Libraries/Yutaka.Core/CSharp/Method.cs
+++ Libraries/Yutaka.Core/CSharp/Method.cs
@@ -8,5 +8,38 @@ namespace Yutaka.Core.CSharp
 {
 	public class Method
 	{
+		public string AccessLevel;
+		public string Modifier;
+		public string ReturnType;
+		public string Name;
+		public string[] Parameters;
+
+		public Method(string accessLevel = null, string modifier = null, string returnType = null, string name = null, string[] parameters = null)
+		{
+			if (String.IsNullOrWhiteSpace(accessLevel))
+				AccessLevel = "public";
+			else
+				AccessLevel = accessLevel.Trim().ToLower();
+
+			if (String.IsNullOrWhiteSpace(modifier))
+				Modifier = null;
+			else
+				Modifier = modifier.Trim().ToLower();
+
+			if (String.IsNullOrWhiteSpace(returnType))
+				ReturnType = "void";
+			else
+				ReturnType = returnType.Trim().ToLower();
+
+			if (String.IsNullOrWhiteSpace(name))
+				throw new ArgumentNullException("name", "<name> is required.");
+			else
+				Name = name.Trim();
+
+			if (parameters == null || parameters.Length == 0)
+				Parameters = null;
+			else
+				Parameters = parameters;
+		}
 	}
 }
