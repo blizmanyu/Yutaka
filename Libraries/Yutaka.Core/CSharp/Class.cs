9c667076ca3784f268f827e12218f1d412499eb7
Yutaka Blizman <blizman2017@gmail.com>
2020-07-06 19:42:35 -0700

    ToString() - finished for now, but haven't tested fully yet

--- Libraries/Yutaka.Core/CSharp/Class.cs
+++ Libraries/Yutaka.Core/CSharp/Class.cs
@@ -56,6 +56,7 @@ namespace Yutaka.Core.CSharp
 		public override string ToString()
 		{
 			var sb = new StringBuilder();
+			CurrentIndentation = "";
 
 			#region Usings
 			if (Usings != null && Usings.Count > 0) {
@@ -70,30 +71,58 @@ namespace Yutaka.Core.CSharp
 			if (!String.IsNullOrWhiteSpace(Namespace)) {
 				sb.AppendLine(String.Format("namespace {0}", Namespace));
 				sb.AppendLine("{");
-				sb.Append("\t");
+				IncreaseIndent();
 			}
 
 			#region Class
-			if (!String.IsNullOrWhiteSpace(AccessLevel))
-				sb.Append(String.Format("{0} ", AccessLevel));
-			if (!String.IsNullOrWhiteSpace(Modifier))
-				sb.Append(String.Format("{0} ", Modifier));
+			sb.AppendFormat("{0}{1}{2}class {3}{4}{5}",
+				CurrentIndentation, // {0}
+				String.IsNullOrWhiteSpace(AccessLevel) ? "" : String.Format("{0} ", AccessLevel), // {1}
+				String.IsNullOrWhiteSpace(Modifier) ? "" : String.Format("{0} ", Modifier), // {2}
+				Name, // {3}
+				String.IsNullOrWhiteSpace(BaseClass) ? "" : String.Format(" : {0}", BaseClass), // {4}
+				Environment.NewLine // {5}
+				);
 
-			sb.Append(String.Format("class {0}", Name));
+			sb.AppendFormat("{0}{{{1}", CurrentIndentation, Environment.NewLine);
+			IncreaseIndent();
 
-			if (!String.IsNullOrWhiteSpace(BaseClass))
-				sb.Append(String.Format(" : {0}", BaseClass));
-
+			#region Constructor
+			sb.AppendFormat("{0}public {2}() {{ }}{1}", CurrentIndentation, Environment.NewLine, Name);
 			sb.AppendLine();
-			sb.AppendLine("{");
+			#endregion Constructor
+
+			#region Fields
+			if (Fields != null && Fields.Count > 0) {
+				var lastField = Fields.Last();
+
+				foreach (var field in Fields) {
+					sb.AppendFormat("{0}{2}{1}", CurrentIndentation, Environment.NewLine, field);
+					if (!field.Equals(lastField))
+						sb.AppendLine();
+				}
+			}
+			#endregion Fields
+
+			#region Methods
+			if (Methods != null && Methods.Count > 0) {
+				var lastMethod = Methods.Last();
+				sb.AppendLine();
+
+				foreach (var method in Methods) {
+					sb.AppendFormat("{0}{2}{1}", CurrentIndentation, Environment.NewLine, method);
+					if (!method.Equals(lastMethod))
+						sb.AppendLine();
+				}
+			}
+			#endregion Methods
+
+			DecreaseIndent();
+			sb.AppendFormat("{0}}}{1}", CurrentIndentation, Environment.NewLine);
 			#endregion Class
 
-
-
-
-
 			if (!String.IsNullOrWhiteSpace(Namespace))
-				sb.AppendLine("}");
+				sb.AppendFormat("}}{0}", Environment.NewLine);
 			#endregion Namespace
 
 			return sb.ToString();
