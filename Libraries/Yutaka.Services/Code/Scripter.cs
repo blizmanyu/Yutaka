b47c9b83f4a5bd537f45d471abcc11221ba0ae32
Yutaka Blizman <blizman2017@gmail.com>
2020-07-08 19:15:08 -0700

    WIP: public string ScriptModel(IList<Column> columns)

--- Libraries/Yutaka.Services/Code/Scripter.cs
+++ Libraries/Yutaka.Services/Code/Scripter.cs
@@ -50,123 +50,162 @@ namespace Yutaka.Code
 			return finalScript.ToString();
 		}
 
-		//public string ScriptModel(IList<Column> columns)
-		//{
-		//	#region Input Check
-		//	var log = "";
+		public string ScriptModel(IList<Column> columns)
+		{
+			#region Input Check
+			var log = "";
 
-		//	if (columns == null)
-		//		log = String.Format("{0}<columns> is null.{1}", log, Environment.NewLine);
-		//	else if (columns.Count == 0)
-		//		log = String.Format("{0}<columns> is empty.{1}", log, Environment.NewLine);
+			if (columns == null)
+				log = String.Format("{0}<columns> is null.{1}", log, Environment.NewLine);
+			else if (columns.Count == 0)
+				log = String.Format("{0}<columns> is empty.{1}", log, Environment.NewLine);
 
-		//	if (!String.IsNullOrWhiteSpace(log)) {
-		//		log = String.Format("{0}Exception thrown in Scripter.ScriptModel(IList<Column> columns).{1}{1}", log, Environment.NewLine);
-		//		return log;
-		//	}
-		//	#endregion
+			if (!String.IsNullOrWhiteSpace(log)) {
+				log = String.Format("{0}Exception thrown in Scripter.ScriptModel(IList<Column> columns).{1}{1}", log, Environment.NewLine);
+				return log;
+			}
+			#endregion
 
-		//	var finalScript = new StringBuilder();
-		//	var checkInputBlock = new StringBuilder();
-		//	var tryBlock = new StringBuilder();
-		//	var catchBlock = new StringBuilder();
-		//	Class cl = null;
-		//	Field field = null;
-		//	List <Field> fields = null;
-		//	var database = "";
-		//	var schema = "";
-		//	var table = "";
-		//	var alias = "";
+			var finalScript = new StringBuilder();
+			var checkInputBlock = new StringBuilder();
+			var tryBlock = new StringBuilder();
+			var catchBlock = new StringBuilder();
+			Class cl = null;
+			Field field = null;
+			List <Field> fields = null;
+			var database = "";
+			var schema = "";
+			var table = "";
+			var alias = "";
 
-		//	columns = columns.OrderBy(x => x.TableSchema).ThenBy(x => x.TableName).ThenBy(x => x.OrdinalPosition).ToList();
+			columns = columns.OrderBy(x => x.TableSchema).ThenBy(x => x.TableName).ThenBy(x => x.OrdinalPosition).ToList();
 
-		//	foreach (var tables in columns.GroupBy(x => new { x.TableCatalog, x.TableSchema, x.TableName })) {
-		//		database = tables.Key.TableCatalog;
-		//		schema = tables.Key.TableSchema;
-		//		table = tables.Key.TableName.Replace(".", "_");
-		//		alias = table.Replace("_", "").Substring(0, 2).ToLower();
+			foreach (var tables in columns.GroupBy(x => new { x.TableCatalog, x.TableSchema, x.TableName })) {
+				database = tables.Key.TableCatalog;
+				schema = tables.Key.TableSchema;
+				table = tables.Key.TableName.Replace(".", "_");
+				alias = table.Replace("_", "").Substring(0, 2).ToLower();
 
-		//		// check for C# keywords //
-		//		if (alias.Equals("in"))
-		//			alias = table.Replace("_", "").Substring(0, 3).ToLower();
+				// check for C# keywords //
+				if (alias.Equals("in"))
+					alias = table.Replace("_", "").Substring(0, 3).ToLower();
 
-		//		cl = new Class {
-		//			AccessLevel = "public",
-		//			BaseClass = null,
-		//			Fields = new List<Field>(),
-		//			Methods = new List<Method>(),
-		//			Modifier = null,
-		//			Name = String.Format("{0}Model", table),
-		//			Namespace = String.Format("Yutaka.Models.{0}s", table),
-		//			Usings = new List<string>(),
-		//		};
+				cl = new Class {
+					AccessLevel = "public",
+					BaseClass = null,
+					Fields = new List<Field>(),
+					Methods = new List<Method>(),
+					Modifier = null,
+					Name = String.Format("{0}Model", table),
+					Namespace = String.Format("Yutaka.Models.{0}s", table),
+					Usings = new List<string>(),
+				};
 
-		//		cl.Usings.Add("System");
+				cl.Usings.Add("System");
 
-		//		foreach (var col in tables) {
-		//			if (!col.IsIdentity && !col.IsComputed) {
-		//				tryBlock.Append(String.Format("\t\t\t\t\tnew SqlParameter(\"@{0}\", {1}.{0}", col.ColumnName, alias));
+				foreach (var col in tables) {
+					field = new Field {
+						AccessLevel = "public",
+						DisplayName = col.ColumnName,
+						Getter = null,
+						IsAutoImplemented = true,
+						Modifier = null,
+						Name = col.ColumnName,
+						Setter = null,
+						Type = null,
+						UIHint = null,
+					};
 
-		//				if (col.IsNullable) {
-		//					if (col.ColumnName.StartsWith("Delete"))
-		//						tryBlock.AppendLine(" ?? null),");
-		//					else if (col.DataType.Equals("bit") || col.DataType.Equals("datetime"))
-		//						tryBlock.AppendLine(" ?? null),");
-		//					else if (col.DataType.Equals("int") || col.DataType.Equals("decimal") || col.DataType.Equals("numeric"))
-		//						tryBlock.AppendLine(" ?? -1),");
-		//					else
-		//						tryBlock.AppendLine(" ?? \"\"),");
-		//				}
+					switch (col.DataType) {
+						#region case "bit":
+						case "bit":
+							if (col.IsNullable)
+								field.Type = "bool?";
+							else
+								field.Type = "bool";
+							break;
+						#endregion case "bit":
+						#region case "datetime":
+						case "datetime":
+							if (col.IsNullable)
+								field.Type = "DateTime?";
+							else
+								field.Type = "DateTime";
+							break;
+						#endregion case "datetime":
+						default:
+							field.Type = col.DataType;
+							break;
+					}
 
-		//				else if (col.DataType.Equals("varchar") || col.DataType.Equals("nvarchar"))
-		//					tryBlock.AppendLine(" ?? \"\"),");
-		//				else
-		//					tryBlock.AppendLine("),");
-		//			}
-		//		}
 
-		//		#region Close Try Block
-		//		tryBlock.AppendLine("\t\t\t\t};");
-		//		tryBlock.AppendLine("\t\t\t\tExecuteStoredProcedure(storProc, parameters);");
-		//		tryBlock.AppendLine("\t\t\t\treturn true;");
-		//		tryBlock.AppendLine("\t\t\t}");
-		//		#endregion Close Try Block
 
-		//		#region Catch Block
-		//		catchBlock = new StringBuilder();
-		//		catchBlock.AppendLine("\t\t\tcatch (Exception ex) {");
-		//		catchBlock.AppendLine("\t\t\t\t#region Log");
-		//		catchBlock.AppendLine("\t\t\t\tif (ex.InnerException == null)");
-		//		catchBlock.AppendLine(String.Format("\t\t\t\t\tresponse = String.Format(\"{{0}}{{2}}Exception thrown in {0}Service.TryInsert{1}({1} {2}, out string response).{{2}}{{1}}{{2}}{{2}}\", ex.Message, ex.ToString(), Environment.NewLine);", database, table, alias));
-		//		catchBlock.AppendLine("\t\t\t\telse");
-		//		catchBlock.AppendLine(String.Format("\t\t\t\t\tresponse = String.Format(\"{{0}}{{2}}Exception thrown in INNER EXCEPTION of {0}Service.TryInsert{1}({1} {2}, out string response).{{2}}{{1}}{{2}}{{2}}\", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine);", database, table, alias));
-		//		catchBlock.AppendLine("\t\t\t\t#endregion Log");
-		//		catchBlock.AppendLine();
-		//		catchBlock.AppendLine("\t\t\t\treturn false;");
-		//		catchBlock.AppendLine("\t\t\t}");
-		//		#endregion Catch Block
 
-		//		method = new Method {
-		//			AccessLevel = "public",
-		//			Body = "",
-		//			Modifier = null,
-		//			Name = String.Format("TryInsert{0}", table),
-		//			Parameters = String.Format("{0} {1}, out string response", table, alias),
-		//			ReturnType = "bool",
-		//		};
 
-		//		if (!String.IsNullOrWhiteSpace(checkInputBlock.ToString()))
-		//			method.Body = String.Format("{0}{1}{2}", method.Body, checkInputBlock, Environment.NewLine);
-		//		if (!String.IsNullOrWhiteSpace(tryBlock.ToString()))
-		//			method.Body = String.Format("{0}{1}{2}", method.Body, tryBlock, Environment.NewLine);
-		//		if (!String.IsNullOrWhiteSpace(catchBlock.ToString()))
-		//			method.Body = String.Format("{0}{1}", method.Body, catchBlock);
 
-		//		finalScript.Append(method.ToString());
-		//	}
+					if (!col.IsIdentity && !col.IsComputed) {
+						tryBlock.Append(String.Format("\t\t\t\t\tnew SqlParameter(\"@{0}\", {1}.{0}", col.ColumnName, alias));
 
-		//	return finalScript.ToString();
-		//}
+						if (col.IsNullable) {
+							if (col.ColumnName.StartsWith("Delete"))
+								tryBlock.AppendLine(" ?? null),");
+							else if (col.DataType.Equals("bit") || col.DataType.Equals("datetime"))
+								tryBlock.AppendLine(" ?? null),");
+							else if (col.DataType.Equals("int") || col.DataType.Equals("decimal") || col.DataType.Equals("numeric"))
+								tryBlock.AppendLine(" ?? -1),");
+							else
+								tryBlock.AppendLine(" ?? \"\"),");
+						}
+
+						else if (col.DataType.Equals("varchar") || col.DataType.Equals("nvarchar"))
+							tryBlock.AppendLine(" ?? \"\"),");
+						else
+							tryBlock.AppendLine("),");
+					}
+				}
+
+				#region Close Try Block
+				tryBlock.AppendLine("\t\t\t\t};");
+				tryBlock.AppendLine("\t\t\t\tExecuteStoredProcedure(storProc, parameters);");
+				tryBlock.AppendLine("\t\t\t\treturn true;");
+				tryBlock.AppendLine("\t\t\t}");
+				#endregion Close Try Block
+
+				#region Catch Block
+				catchBlock = new StringBuilder();
+				catchBlock.AppendLine("\t\t\tcatch (Exception ex) {");
+				catchBlock.AppendLine("\t\t\t\t#region Log");
+				catchBlock.AppendLine("\t\t\t\tif (ex.InnerException == null)");
+				catchBlock.AppendLine(String.Format("\t\t\t\t\tresponse = String.Format(\"{{0}}{{2}}Exception thrown in {0}Service.TryInsert{1}({1} {2}, out string response).{{2}}{{1}}{{2}}{{2}}\", ex.Message, ex.ToString(), Environment.NewLine);", database, table, alias));
+				catchBlock.AppendLine("\t\t\t\telse");
+				catchBlock.AppendLine(String.Format("\t\t\t\t\tresponse = String.Format(\"{{0}}{{2}}Exception thrown in INNER EXCEPTION of {0}Service.TryInsert{1}({1} {2}, out string response).{{2}}{{1}}{{2}}{{2}}\", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine);", database, table, alias));
+				catchBlock.AppendLine("\t\t\t\t#endregion Log");
+				catchBlock.AppendLine();
+				catchBlock.AppendLine("\t\t\t\treturn false;");
+				catchBlock.AppendLine("\t\t\t}");
+				#endregion Catch Block
+
+				method = new Method {
+					AccessLevel = "public",
+					Body = "",
+					Modifier = null,
+					Name = String.Format("TryInsert{0}", table),
+					Parameters = String.Format("{0} {1}, out string response", table, alias),
+					ReturnType = "bool",
+				};
+
+				if (!String.IsNullOrWhiteSpace(checkInputBlock.ToString()))
+					method.Body = String.Format("{0}{1}{2}", method.Body, checkInputBlock, Environment.NewLine);
+				if (!String.IsNullOrWhiteSpace(tryBlock.ToString()))
+					method.Body = String.Format("{0}{1}{2}", method.Body, tryBlock, Environment.NewLine);
+				if (!String.IsNullOrWhiteSpace(catchBlock.ToString()))
+					method.Body = String.Format("{0}{1}", method.Body, catchBlock);
+
+				finalScript.Append(method.ToString());
+			}
+
+			return finalScript.ToString();
+		}
 
 		/// <summary>
 		/// Creates script for a TryInsert method.
