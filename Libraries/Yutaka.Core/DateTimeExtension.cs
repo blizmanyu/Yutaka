e137f4f7a08149e43a8920856683053e2efd63e5
Yutaka Blizman <blizman2017@gmail.com>
2020-08-25 20:00:30 -0700

    New public static class DateTimeExtension.cs

---
+++ Libraries/Yutaka.Core/DateTimeExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Yutaka
+{
+	public static class DateTimeExtension
+	{
+		public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Sunday)
+		{
+			var diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+			return dt.AddDays(-diff).Date;
+		}
+
+		public static DateTime EndOfWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Sunday)
+		{
+			return StartOfWeek(dt, startOfWeek).AddDays(6);
+		}
+
+		public static DateTime StartOfYear(this DateTime dt)
+		{
+			return new DateTime(dt.Year, 1, 1);
+		}
+
+		public static DateTime EndOfYear(this DateTime dt)
+		{
+			return new DateTime(dt.Year, 12, 31);
+		}
+	}
+}
