6dde92cc8f2d507a6907321120b30e83524c13eb
Yutaka Blizman <blizman2017@gmail.com>
2020-09-29 19:38:27 -0700

    New method public static IList<string> GetImageFiles(string path)

--- Libraries/Yutaka.Core/IO/DirectoryUtil.cs
+++ Libraries/Yutaka.Core/IO/DirectoryUtil.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Yutaka.Core.IO
 {
@@ -51,9 +52,59 @@ namespace Yutaka.Core.IO
 
 			catch (Exception ex) {
 				if (ex.InnerException == null)
-					throw new Exception(String.Format("{0}{2}Exception thrown in DirectoryUtil.EnumerateFiles(string path='{3}', string searchPattern='{4}').{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, path, searchPattern));
+					throw new Exception(String.Format("{0}{2}Exception thrown in DirectoryUtil.GetFiles(string path='{3}', string searchPattern='{4}').{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, path, searchPattern));
 				else
-					throw new Exception(String.Format("{0}{2}Exception thrown in INNER EXCEPTION of DirectoryUtil.EnumerateFiles(string path='{3}', string searchPattern='{4}').{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, path, searchPattern));
+					throw new Exception(String.Format("{0}{2}Exception thrown in INNER EXCEPTION of DirectoryUtil.GetFiles(string path='{3}', string searchPattern='{4}').{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, path, searchPattern));
+			}
+		}
+
+		public static IList<string> GetImageFiles(string path)
+		{
+			#region Check Input
+			if (String.IsNullOrWhiteSpace(path))
+				throw new ArgumentException("'path' is null or whitespace.", "path");
+			if (!Directory.Exists(path))
+				throw new ArgumentException(String.Format("'path' '{0}' does not exist.", path), "path");
+			#endregion Check Input
+
+			try {
+				string currentDir;
+				var files = new List<string>();
+				var dirs = new Stack<string>(InitialStackCapacity);
+				dirs.Push(path);
+
+				while (dirs.Count > 0) {
+					currentDir = dirs.Pop();
+
+					try {
+						files.AddRange(Directory.EnumerateFiles(currentDir).Where(x => x.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".gif", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".ico", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".tiff", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".wdp", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".webm", StringComparison.OrdinalIgnoreCase) ||
+																					   x.EndsWith(".webp", StringComparison.OrdinalIgnoreCase)));
+
+						foreach (var dir in Directory.EnumerateDirectories(currentDir))
+							dirs.Push(dir);
+					}
+
+					catch (Exception) {
+						continue;
+					}
+				}
+
+				return files;
+			}
+
+			catch (Exception ex) {
+				if (ex.InnerException == null)
+					throw new Exception(String.Format("{0}{2}Exception thrown in DirectoryUtil.GetImageFiles(string path='{3}').{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, path));
+				else
+					throw new Exception(String.Format("{0}{2}Exception thrown in INNER EXCEPTION of DirectoryUtil.GetImageFiles(string path='{3}').{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, path));
 			}
 		}
 	}
