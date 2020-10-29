af4c9deddf817a818a04d672b51b4e877128de93
Yutaka Blizman <blizman2017@gmail.com>
2020-10-28 20:09:02 -0700

    WIP: New public classes FileUtil.cs & WinDirStat.cs

---
+++ Libraries/Yutaka.Core/IO/FileUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yutaka.Core.IO
+{
+	public static class FileUtil
+	{
+		private static readonly string[] _sizes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB" };
+
+		//public static string BytesToString(long bytes)
+		//{
+		//	var order = 0;
+
+		//	while (bytes >= 1024 && order < _sizes.Length - 1) {
+		//		++order;
+		//		bytes = bytes / 1024;
+		//	}
+
+		//	// Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+		//	// show a single decimal place, and no space.
+		//	string result = String.Format("{0:0.##} {1}", bytes, _sizes[order]);
+		//}
+	}
+}
---
+++ Libraries/Yutaka.Core/IO/WinDirStat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Yutaka.Core.IO
+{
+	public class WinDirStat
+	{
+		public string Name;
+		public long Size;
+		public int Items;
+		public int Files;
+		public int Subdirs;
+		public DateTime MinDate;
+		public DateTime MaxDate;
+
+		//public string SizeStr
+		//{
+		//	get {
+		//		return Fuel;
+		//	}
+		//}
+	}
+}
