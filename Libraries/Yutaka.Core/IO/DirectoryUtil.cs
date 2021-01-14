a1cf30f1f1a43d9c1336b0e2f741bcca5ba3fd67
Yutaka Blizman <blizman2017@gmail.com>
2021-01-13 18:56:00 -0800

    EnumerateImageFiles() now allows for SearchOption

--- Libraries/Yutaka.Core/IO/DirectoryUtil.cs
+++ Libraries/Yutaka.Core/IO/DirectoryUtil.cs
@@ -67,54 +67,73 @@ namespace Yutaka.Core.IO
 		}
 
 		/// <summary>
-		/// Returns an enumerable collection of full image file names that match a search pattern in a specified path and subdirectories.
+		/// Returns an enumerable collection of full image file names in a specified path, and optionally searches subdirectories.
 		/// </summary>
 		/// <param name="path">The relative or absolute path to the directory to search. This string is not case-sensitive.</param>
-		/// <returns>An enumerable collection of the full names (including paths) for the image files in the directory specified by &lt;path&gt;.</returns>
-		public static IEnumerable<string> EnumerateImageFiles(string path)
+		/// <param name="searchOption">One of the enumeration values that specifies whether the search operation should include only the current directory or should include all subdirectories. The default value is <see cref="SearchOption.TopDirectoryOnly"/>.</param>
+		/// <returns>An enumerable collection of the full names (including paths) for the image files in the directory specified by &lt;path&gt; and that match the specified search option.</returns>
+		public static IEnumerable<string> EnumerateImageFiles(string path, SearchOption searchOption = SearchOption.TopDirectoryOnly)
 		{
 			#region Check Input
-			if (String.IsNullOrWhiteSpace(path))
-				throw new ArgumentException("'path' is null or whitespace.", "path");
+			if (path == null)
+				throw new ArgumentNullException("path");
+			else if (String.IsNullOrWhiteSpace(path))
+				throw new ArgumentException("'path' is whitespace.", "path");
 			if (!Directory.Exists(path))
-				throw new ArgumentException(String.Format("'path' '{0}' does not exist.", path), "path");
+				throw new ArgumentException(String.Format("Path '{0}' does not exist.", path), "path");
 			#endregion Check Input
 
-			IEnumerable<string> files;
-			string currentDir;
-			var dirs = new Stack<string>(InitialStackCapacity);
-			dirs.Push(path);
-
-			while (dirs.Count > 0) {
-				currentDir = dirs.Pop();
-
-				try {
-					files = Directory.EnumerateFiles(currentDir).Where(x => x.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".gif", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".ico", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".tiff", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".wdp", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".webm", StringComparison.OrdinalIgnoreCase) ||
-																			x.EndsWith(".webp", StringComparison.OrdinalIgnoreCase));
-				}
-
-				catch {
-					continue;
-				}
-
-				foreach (var file in files)
+			if (searchOption == SearchOption.TopDirectoryOnly) {
+				foreach (var file in Directory.EnumerateFiles(path).Where(x => x.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".gif", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".ico", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".tiff", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".wdp", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".webm", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".webp", StringComparison.OrdinalIgnoreCase)))
 					yield return file;
+			}
 
-				try {
-					foreach (var dir in Directory.EnumerateDirectories(currentDir))
-						dirs.Push(dir);
-				}
+			else {
+				IEnumerable<string> files;
+				string currentDir;
+				var dirs = new Stack<string>(InitialStackCapacity);
+				dirs.Push(path);
 
-				catch {
-					continue;
+				while (dirs.Count > 0) {
+					currentDir = dirs.Pop();
+
+					try {
+						files = Directory.EnumerateFiles(currentDir).Where(x => x.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".gif", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".ico", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".tiff", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".wdp", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".webm", StringComparison.OrdinalIgnoreCase) ||
+																				x.EndsWith(".webp", StringComparison.OrdinalIgnoreCase));
+					}
+
+					catch {
+						continue;
+					}
+
+					foreach (var file in files)
+						yield return file;
+
+					try {
+						foreach (var dir in Directory.EnumerateDirectories(currentDir))
+							dirs.Push(dir);
+					}
+
+					catch {
+						continue;
+					}
 				}
 			}
 		}
