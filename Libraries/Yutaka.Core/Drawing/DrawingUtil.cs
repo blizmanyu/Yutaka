ea0983476b80daa7d93fa4613be3c66f81c5b5b0
Yutaka Blizman <blizman2017@gmail.com>
2021-01-18 20:00:14 -0800

    New method public static Bitmap ScaleImage(string imagePath, int maxWidth, int maxHeight)

--- Libraries/Yutaka.Core/Drawing/DrawingUtil.cs
+++ Libraries/Yutaka.Core/Drawing/DrawingUtil.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.IO;
 
 namespace Yutaka.Core.Drawing
 {
@@ -52,6 +53,46 @@ namespace Yutaka.Core.Drawing
 			var newHeight = Math.Round(image.Height * ratio, MidpointRounding.AwayFromZero);
 
 			return ResizeImage(image, (int) newWidth, (int) newHeight);
+		}
+
+		/// <summary>
+		/// Resizes an image, keeping the same aspect ratio.
+		/// </summary>
+		/// <param name="imagePath">The path to the image to resize.</param>
+		/// <param name="maxWidth">The max width to resize to.</param>
+		/// <param name="maxHeight">The max height to resize to.</param>
+		/// <returns>The resized image.</returns>
+		public static Bitmap ScaleImage(string imagePath, int maxWidth, int maxHeight)
+		{
+			#region Check Input
+			if (imagePath == null)
+				throw new ArgumentNullException("imagePath");
+			else if (String.IsNullOrWhiteSpace(imagePath))
+				throw new ArgumentException("'imagePath' is empty or whitespace.", "imagePath");
+			if (!File.Exists(imagePath))
+				throw new FileNotFoundException("File not found.", imagePath);
+			#endregion Check Input
+
+			try {
+				using (var fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read, FileShare.Read)) {
+					using (var image = Image.FromStream(fileStream, false, false)) {
+						var ratioX = (decimal) maxWidth / image.Width;
+						var ratioY = (decimal) maxHeight / image.Height;
+						var ratio = Math.Min(ratioX, ratioY);
+						var newWidth = Math.Round(image.Width * ratio, MidpointRounding.AwayFromZero);
+						var newHeight = Math.Round(image.Height * ratio, MidpointRounding.AwayFromZero);
+
+						return ResizeImage(image, (int) newWidth, (int) newHeight);
+					}
+				}
+			}
+
+			catch (Exception ex) {
+				if (ex.InnerException == null)
+					throw new Exception(String.Format("{0}{2}Exception thrown in DrawingUtil.ScaleImage(string imagePath='{3}', int maxWidth='{4}', int maxHeight='{5}').{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, imagePath, maxWidth, maxHeight));
+				else
+					throw new Exception(String.Format("{0}{2}Exception thrown in INNER EXCEPTION of DrawingUtil.ScaleImage(string imagePath='{3}', int maxWidth='{4}', int maxHeight='{5}').{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, imagePath, maxWidth, maxHeight));
+			}
 		}
 	}
 }
