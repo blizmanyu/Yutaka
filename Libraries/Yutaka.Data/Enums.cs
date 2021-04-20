c9377e1061181e7630b154a7178187cc63bdf81f
blizmanyu <blizman2017@gmail.com>
2021-04-20 13:30:27 -0700

    New file Enums.cs

---
+++ Libraries/Yutaka.Data/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace Yutaka.Data
+{
+	/// <summary>
+	/// Specifies how a command string is interpreted.
+	/// </summary>
+	public enum CmdType
+	{
+		/// <summary>
+		/// An SQL text command. (Default.)
+		/// </summary>
+		Text = CommandType.Text,
+		/// <summary>
+		/// The name of a stored procedure.
+		/// </summary>
+		StoredProcedure = CommandType.StoredProcedure,
+		/// <summary>
+		/// The name of a table.
+		/// </summary>
+		TableDirect = CommandType.TableDirect
+	}
+}
