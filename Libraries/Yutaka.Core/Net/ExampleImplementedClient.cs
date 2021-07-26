dd4dc7242756dd6282be814810f7ece2a34bd837
blizmanyu <blizman2017@gmail.com>
2021-07-26 14:12:15 -0700

    New public class ExampleImplementedClient : BaseClient

---
+++ Libraries/Yutaka.Core/Net/ExampleImplementedClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Yutaka.Core.Net
+{
+	/// <summary>
+	/// Example service client that implements BaseClient. Don't actually use this class in any production code.
+	/// </summary>
+	public class ExampleImplementedClient : BaseClient
+	{
+		public static readonly string endpoint = "https://www.google.com";
+
+		public async Task SendRequestToEndpoint()
+		{
+			for (var count = 0; count < 10; count++)
+				await Client.GetStringAsync(endpoint);
+		}
+	}
+}
