82315ca5751974a38b9e2b8e898a6988ba039022
blizmanyu <blizman2017@gmail.com>
2021-07-26 20:44:43 -0700

    New method public static bool TryDelete(string path)

--- Libraries/Yutaka.Core/IO/FileUtil.cs
+++ Libraries/Yutaka.Core/IO/FileUtil.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,30 @@ namespace Yutaka.Core.IO
 {
 	public static class FileUtil
 	{
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool DeleteFile(string lpFileName);
+
 		private static readonly string[] _sizes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB" };
+
+		/// <summary>
+		/// Tries to deletes the specified file.
+		/// </summary>
+		/// <param name="path">The name of the file to be deleted. Wildcard characters are not supported.</param>
+		/// <returns></returns>
+		public static bool TryDelete(string path)
+		{
+			if (String.IsNullOrWhiteSpace(path))
+				return true;
+
+			try {
+				return DeleteFile(path);
+			}
+
+			catch {
+				return false;
+			}
+		}
 
 		//public static string BytesToString(long bytes)
 		//{
