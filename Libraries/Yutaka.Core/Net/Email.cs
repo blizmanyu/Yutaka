56492dde60c3de6380bfeea6d7272134faa8c842
blizmanyu <blizman2017@gmail.com>
2021-08-03 14:16:16 -0700

    Made all fields get-only. Added Summaries

--- Libraries/Yutaka.Core/Net/Email.cs
+++ Libraries/Yutaka.Core/Net/Email.cs
@@ -6,10 +6,36 @@ namespace Yutaka.Core.Net
 	public class Email
 	{
 		#region Fields
-		public string Address;
-		public string DisplayName;
-		public string Host;
-		public string User;
+		/// <summary>
+		/// Gets the email address specified when this instance was created.
+		/// </summary>
+		/// <remarks>The value returned by this property does not include the DisplayName information.</remarks>
+		public string Address { get; }
+		/// <summary>
+		/// Gets the display name composed from the display name and address information specified when this instance was created.
+		/// </summary>
+		/// <remarks>
+		/// Display names that contain non-ASCII characters are returned in human-readable form.
+		/// Use the ToString method to get the encoded form of the DisplayName.
+		/// Some software programs that are used to read email display the DisplayName property value instead of, or in addition to, the email address.
+		/// </remarks>
+		public string DisplayName { get; }
+		/// <summary>
+		/// Gets the host portion of the address specified when this instance was created.
+		/// </summary>
+		/// <remarks>
+		/// In a typical email address, the host string includes all information following the "@" sign.
+		/// For example, in "tsmith@contoso.com", the host is "contoso.com".
+		/// </remarks>
+		public string Host { get; }
+		/// <summary>
+		/// Gets the user information from the address specified when this instance was created.
+		/// </summary>
+		/// <remarks>
+		/// In a typical email address, the user string includes all information preceding the "@" sign.
+		/// For example, in "tsmith@contoso.com", the user is "tsmith".
+		/// </remarks>
+		public string User { get; }
 		#endregion Fields
 
 		#region Constructors
