61fd8e30a696f296adc07d8ea3a369b58e94efe0
blizmanyu <blizman2017@gmail.com>
2021-10-18 15:06:53 -0700

    New public abstract class BaseClient

--- Libraries - 3rd Party/Yutaka.VineSpring/VineSpringClient.cs
+++ Libraries - 3rd Party/Yutaka.VineSpring/VineSpringClient.cs
@@ -1,44 +1,103 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Yutaka.IO;
+using Yutaka.VineSpring.Data;
+using Yutaka.VineSpring.Data20191126;
+using Yutaka.VineSpring.Data20200207;
 
 namespace Yutaka.VineSpring
 {
-	public class VineSpringClient : IDisposable
+	/// <summary>
+	/// A VineSpringClient class.
+	/// </summary>
+	public class VineSpringClient : BaseClient
 	{
-		private static object _locker = new object();
-		private static volatile HttpClient _client;
-		public static readonly Uri BaseAddress = new Uri("https://api.vinespring.com/");
+		#region Fields
+		public const string TIME_FORMAT = @"yyyy-MM-ddT00:00:00.000Z";
+		private static readonly DateTime DOB_THRESHOLD = DateTime.Now.AddYears(-100);
+		public static readonly DateTime MIN_DATE = DateTime.Now.AddYears(-10);
+		public static readonly DateTime MAX_DATE = DateTime.Now.AddYears(1);
+		public string ApiKey;
+		#endregion Fields
 
-		protected static HttpClient Client
+		#region Constructor
+		public VineSpringClient(string apiKey = null)
 		{
-			get {
-				if (_client == null) {
-					lock (_locker) {
-						if (_client == null) {
-							_client = new HttpClient { BaseAddress = BaseAddress };
-							_client.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
-						}
-					}
+			if (String.IsNullOrWhiteSpace(apiKey))
+				throw new Exception(String.Format("'apiKey' is required. Exception thrown in Constructor VineSpringClient(string apiKey = null).{0}", Environment.NewLine));
+
+			ApiKey = apiKey;
+		}
+		#endregion Constructor
+
+		#region Utilities
+		public void WriteToConsole(Task<string> response, bool pretty = true)
+		{
+			if (response == null || String.IsNullOrWhiteSpace(response.Result))
+				return;
+
+			if (pretty) {
+				dynamic parsedJson = JsonConvert.DeserializeObject(response.Result);
+				var formatted = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+				Console.Write("\n{0}", formatted);
+			}
+
+			else
+				Console.Write("\n{0}", response.Result);
+		}
+
+		public void WriteToFile(Task<string> response, bool pretty = true)
+		{
+			if (response == null || String.IsNullOrWhiteSpace(response.Result))
+				return;
+
+			var filename = String.Format("{0}.json", DateTime.Now.ToString("yyyy MMdd HHmm ssff"));
+
+			if (pretty) {
+				dynamic parsedJson = JsonConvert.DeserializeObject(response.Result);
+				var formatted = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+				new FileUtil().Write(formatted, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), filename));
+			}
+
+			else
+				new FileUtil().Write(response.Result, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), filename));
+		}
+		#endregion Utilities
+
+		#region Methods
+		#region Clubs
+		public async Task<string> ListAllClubs()
+		{
+			try {
+				var endpoint = "clubs";
+				Client.DefaultRequestHeaders.TryAddWithoutValidation("x-api-key", ApiKey);
+
+				using (var response = await Client.GetAsync(endpoint)) {
+					var responseData = await response.Content.ReadAsStringAsync();
+					return responseData;
 				}
+			}
 
-				return _client;
+			catch (Exception ex) {
+				#region Log
+				string log;
+
+				if (ex.InnerException == null)
+					log = String.Format("{0}{2}Exception thrown in VineSpringClient.ListAllClubs().{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine);
+				else
+					log = String.Format("{0}{2}Exception thrown in INNER EXCEPTION of VineSpringClient.ListAllClubs().{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine);
+
+				Console.Write("\n{0}", log);
+				throw new Exception(log);
+				#endregion Log
 			}
 		}
-
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-
-		public virtual void Dispose(bool disposing)
-		{
-			if (disposing) {
-				if (_client != null)
-					_client.Dispose();
-
-				_client = null;
-			}
-		}
+		#endregion Clubs
+		#endregion Methods
 	}
 }
