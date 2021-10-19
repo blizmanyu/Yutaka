f8847a4a19d9a7dd0fe0eb4ec191091d72138b3a
blizmanyu <blizman2017@gmail.com>
2021-10-18 18:12:54 -0700

    Moved Club.cs to Domain.Club. New public class ClubMembership.cs

---
+++ Libraries - 3rd Party/Yutaka.VineSpring/Domain/Club/ClubMembership.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Yutaka.VineSpring.Domain.Club
+{
+	public class ClubMembership
+	{
+		public DateTime updatedOn { get; set; }
+		public string accountId { get; set; }
+		public string purchaserCustomerId { get; set; }
+		public string status { get; set; }
+		public string updatedBy { get; set; }
+		public string source { get; set; }
+		public int shipmentsCreated { get; set; }
+		public string clubId { get; set; }
+		public string cardId { get; set; }
+		public bool isGift { get; set; }
+		public string recipientCustomerId { get; set; }
+		public DateTime createdOn { get; set; }
+		public string id { get; set; }
+		public string shippingAddressId { get; set; }
+		public DateTime memberSince { get; set; }
+		public DateTime holdOn { get; set; }
+		public Note note { get; set; }
+		public DateTime cancelationOn { get; set; }
+		public DateTime holdUntil { get; set; }
+		public string cancelationReason { get; set; }
+		public object shipmentsGifted { get; set; }
+		public string shippingMethodId { get; set; }
+		public string salesrep { get; set; }
+		public Customernote customerNote { get; set; }
+		public string custom1 { get; set; }
+	}
+
+	public class Note
+	{
+		public string message { get; set; }
+		public string updatedBy { get; set; }
+		public DateTime updatedOn { get; set; }
+	}
+
+	public class Customernote
+	{
+		public string message { get; set; }
+		public string updatedBy { get; set; }
+		public DateTime updatedOn { get; set; }
+	}
+}
