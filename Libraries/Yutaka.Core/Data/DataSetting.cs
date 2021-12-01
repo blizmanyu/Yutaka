6a6ac881ed87189e902c91c078b50a905c280747
blizmanyu <blizman2017@gmail.com>
2021-11-30 20:32:04 -0800

    New class DataSetting.cs

---
+++ Libraries/Yutaka.Core/Data/DataSetting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yutaka.Core.Data
+{
+	/// <summary>
+	/// Represents a <see cref="DataSetting"/> (connection string information).
+	/// </summary>
+	public partial class DataSetting
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DataSetting"/> class.
+		/// </summary>
+		public DataSetting()
+		{
+			RawDataSetting = new Dictionary<string, string>();
+		}
+
+		/// <summary>
+		/// Gets or sets the Data Provider.
+		/// </summary>
+		public string DataProvider { get; set; }
+
+		/// <summary>
+		/// Gets or sets the Connection String.
+		/// </summary>
+		public string DataConnectionString { get; set; }
+
+		/// <summary>
+		/// Gets the raw DataSetting.
+		/// </summary>
+		public IDictionary<string, string> RawDataSetting { get; private set; }
+
+		/// <summary>
+		/// Indicates whether the entered information is valid.
+		/// </summary>
+		/// <returns></returns>
+		public bool IsValid()
+		{
+			return !String.IsNullOrEmpty(DataProvider) && !String.IsNullOrEmpty(DataConnectionString);
+		}
+	}
+}
