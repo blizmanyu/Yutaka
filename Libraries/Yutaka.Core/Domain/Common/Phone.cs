f8902d9537c523f6b82bb76b17e9b6051208acbf
blizmanyu <blizman2017@gmail.com>
2021-12-27 19:08:18 -0800

    Simplified Fields

--- Libraries/Yutaka.Core/Domain/Common/Phone.cs
+++ Libraries/Yutaka.Core/Domain/Common/Phone.cs
@@ -1,29 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Yutaka.Core.Domain.Common
 {
 	public class Phone
 	{
-		public DateTime CreatedOnUtc;
-		public int CreatedById;
-		public int Id;
-		public int ContactId;
+		#region Fields
+		protected string AreaCode;
+		protected string BaseNumber;
+		protected string Extension;
 
-		public bool IsPrimary;
 		public string Label;
 		public string Number;
-		public string Ext;
-
-		public int UpdatedById;
-		public DateTime UpdatedOnUtc;
-
-		//public Phone(string number = null, string ext = null, string label = null, bool isPrimary = null)
-		//{
-
-		//}
+		/// <summary>
+		/// Gets the minified phone number.
+		/// </summary>
+		public string NumberMinified { get; }
+		/// <summary>
+		/// Gets the "pretty" formatted phone number.
+		/// </summary>
+		public string NumberPretty { get; }
+		#endregion Fields
 	}
 }
