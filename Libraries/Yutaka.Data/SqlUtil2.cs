290ec5cfff3e5cd44f0349f3ebc27649ddcc6c0c
blizmanyu <blizman2017@gmail.com>
2022-01-10 16:21:56 -0800

    New field public static readonly DateTime SqlDateTime_MinValue

--- Libraries/Yutaka.Data/SqlUtil2.cs
+++ Libraries/Yutaka.Data/SqlUtil2.cs
@@ -16,6 +16,7 @@ namespace Yutaka.Data
 		/// The name of a stored procedure.
 		/// </summary>
 		public static readonly CommandType CommandType_StoredProcedure = CommandType.StoredProcedure;
+		public static readonly DateTime SqlDateTime_MinValue = new DateTime(1753, 1, 1);
 		#endregion Fields
 
 		#region Constructor
