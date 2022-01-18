74602ae1444e6bf93b0e5a34c4c1bda880a8caca
blizmanyu <blizman2017@gmail.com>
2022-01-17 17:59:14 -0800

    New public class ListAllAddressesResponse.cs

---
+++ Libraries - 3rd Party/Yutaka.VineSpring/Domain/Customer/ListAllAddressesResponse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Yutaka.VineSpring.Domain.Customer
+{
+	public class ListAllAddressesResponse
+	{
+		[JsonProperty("isInternational")]
+		public bool IsInternational { get; set; }
+
+		[JsonProperty("updatedOn")]
+		public DateTime UpdatedOn { get; set; }
+
+		[JsonProperty("label")]
+		public string Label { get; set; }
+
+		[JsonProperty("line1")]
+		public string Line1 { get; set; }
+
+		[JsonProperty("line2")]
+		public string Line2 { get; set; }
+
+		[JsonProperty("updatedBy")]
+		public string UpdatedBy { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("state")]
+		public string State { get; set; }
+
+		[JsonProperty("city")]
+		public string City { get; set; }
+
+		[JsonProperty("createdOn")]
+		public DateTime CreatedOn { get; set; }
+
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("phone")]
+		public string Phone { get; set; }
+
+		[JsonProperty("postalCode")]
+		public string PostalCode { get; set; }
+
+		[JsonProperty("customerId")]
+		public string CustomerId { get; set; }
+
+		[JsonProperty("country")]
+		public string Country { get; set; }
+
+		[JsonProperty("organization")]
+		public string Organization { get; set; }
+	}
+}
