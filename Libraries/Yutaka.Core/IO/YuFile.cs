5fd40157e6e93db0259e8802d12db1de6a71bb30
Yutaka Blizman <blizman2017@gmail.com>
2022-01-30 03:08:45 -0800

    New field public DateTime? DateTaken

--- Libraries/Yutaka.Core/IO/YuFile.cs
+++ Libraries/Yutaka.Core/IO/YuFile.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Yutaka.Core.IO
 {
 	public class YuFile
 	{
 		#region Fields
+		protected const int PROPERTY_TAG_EXIF_DATE_TAKEN = 36867; // PropertyTagExifDTOrig // https://docs.microsoft.com/en-us/dotnet/api/system.drawing.imaging.propertyitem.id //
 		protected static readonly DateTime DateThreshold = new DateTime(1960, 1, 1);
 		protected static readonly DateTime TwoYearsAgo = DateTime.Today.AddYears(-2);
+		protected static readonly Regex Regex_Colon = new Regex(":", RegexOptions.Compiled);
 		/// <summary>
 		/// The path originally specified by the user, whether relative or absolute.
 		/// </summary>
@@ -20,6 +25,18 @@ namespace Yutaka.Core.IO
 		public DateTime LastAccessTimeUtc { get; set; }
 		public DateTime LastWriteTime { get; set; }
 		public DateTime LastWriteTimeUtc { get; set; }
+		#region public DateTime? DateTaken { }
+		public DateTime? DateTaken {
+			get {
+				if (DateTaken == null)
+					SetDateTaken();
+
+				return DateTaken;
+			} 
+
+			private set { }
+		}
+		#endregion
 		/// <summary>
 		/// Gets the size, in bytes, of the current file.
 		/// </summary>
@@ -160,6 +177,7 @@ namespace Yutaka.Core.IO
 			Console.Write("\nLastAccessTimeUtc: {0:yyyy-MM-dd HH:mm:ss.fff}", LastAccessTimeUtc);
 			Console.Write("\n    LastWriteTime: {0:yyyy-MM-dd HH:mm:ss.fff}", LastWriteTime);
 			Console.Write("\n LastWriteTimeUtc: {0:yyyy-MM-dd HH:mm:ss.fff}", LastWriteTimeUtc);
+			Console.Write("\n        DateTaken: {0:yyyy-MM-dd HH:mm:ss.fff}", DateTaken);
 			Console.Write("\n             Size: {0:n0}", Size);
 			Console.Write("\n        Extension: {0}", Extension);
 			Console.Write("\nFullDirectoryPath: {0}", FullDirectoryPath);
@@ -168,6 +186,30 @@ namespace Yutaka.Core.IO
 			Console.Write("\n  ParentDirectory: {0}", ParentDirectory);
 			Console.Write("\n             Root: {0}", Root);
 			Console.Write("\n");
+		}
+
+		/// <summary>
+		/// Sets the DateTaken WITHOUT loading the whole image.
+		/// </summary>
+		protected void SetDateTaken()
+		{
+			try {
+				using (var fs = new FileStream(FullPath, FileMode.Open, FileAccess.Read)) {
+					using (var img = Image.FromStream(fs, false, false)) {
+						var propItem = img.GetPropertyItem(PROPERTY_TAG_EXIF_DATE_TAKEN);
+						var dateTaken = Regex_Colon.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+
+						if (DateTime.TryParse(dateTaken, out var result))
+							DateTaken = result;
+						else
+							DateTaken = null;
+					}
+				}
+			}
+
+			catch (Exception) {
+				DateTaken = null;
+			}
 		}
 		#endregion
 	}
