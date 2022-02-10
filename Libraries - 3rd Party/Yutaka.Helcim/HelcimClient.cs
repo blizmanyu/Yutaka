3602bbd2ed9bed36ab8723108527c76fe0206beb
Yutaka Blizman <blizman2017@gmail.com>
2022-02-09 22:34:04 -0800

    Added base structure

--- Libraries - 3rd Party/Yutaka.Helcim/HelcimClient.cs
+++ Libraries - 3rd Party/Yutaka.Helcim/HelcimClient.cs
@@ -1,12 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Yutaka.Core.Net;
+using Yutaka.IO;
 
 namespace Yutaka.Helcim
 {
-	public class HelcimClient
+	public class HelcimClient : BaseClient
 	{
+		#region Fields
+		public int AccountId;
+		public string ApiToken;
+		#endregion Fields
+
+		#region Constructor
+		public HelcimClient(int accountId, string apiToken)
+		{
+			if (accountId < 1)
+				throw new Exception(String.Format("'accountId' is invalid.{0}Exception thrown in Constructor HelcimClient(int accountId, string apiToken).{0}", Environment.NewLine));
+			if (String.IsNullOrWhiteSpace(apiToken))
+				throw new Exception(String.Format("'apiToken' is required.{0}Exception thrown in Constructor HelcimClient(int accountId, string apiToken).{0}", Environment.NewLine));
+
+			AccountId = accountId;
+			ApiToken = apiToken;
+		}
+		#endregion Constructor
+
+		#region Utilities
+		#region Writes
+		public void WriteToConsole(Task<string> response, bool pretty = true)
+		{
+			if (response == null || String.IsNullOrWhiteSpace(response.Result))
+				return;
+
+			if (pretty) {
+				dynamic parsedJson = JsonConvert.DeserializeObject(response.Result);
+				var formatted = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+				Console.Write("\n{0}", formatted);
+			}
+
+			else
+				Console.Write("\n{0}", response.Result);
+		}
+
+		public void WriteToFile(string response, bool pretty = true)
+		{
+			if (response == null || String.IsNullOrWhiteSpace(response))
+				return;
+
+			var filename = String.Format("{0}.json", DateTime.Now.ToString("yyyy MMdd HHmm ssff"));
+			var folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "JSON");
+			Directory.CreateDirectory(folder);
+
+			if (pretty) {
+				dynamic parsedJson = JsonConvert.DeserializeObject(response);
+				var formatted = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+				new FileUtil().Write(formatted, Path.Combine(folder, filename));
+			}
+
+			else
+				new FileUtil().Write(response, Path.Combine(folder, filename));
+		}
+
+		public void WriteToFile(Task<string> response, bool pretty = true)
+		{
+			if (response == null || String.IsNullOrWhiteSpace(response.Result))
+				return;
+
+			var filename = String.Format("{0}.json", DateTime.Now.ToString("yyyy MMdd HHmm ssff"));
+			var folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "JSON");
+			Directory.CreateDirectory(folder);
+
+			if (pretty) {
+				dynamic parsedJson = JsonConvert.DeserializeObject(response.Result);
+				var formatted = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+				new FileUtil().Write(formatted, Path.Combine(folder, filename));
+			}
+
+			else
+				new FileUtil().Write(response.Result, Path.Combine(folder, filename));
+		}
+		#endregion Writes
+		#endregion Utilities
+
+		#region Methods
+		#endregion Methods
 	}
 }
