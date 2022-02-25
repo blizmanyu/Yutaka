3630dfd8fa5f0891b8ae3b0cc2203cd367950c56
blizmanyu <blizman2017@gmail.com>
2022-02-24 16:53:19 -0800

    Changed CommandType_Text & CommandType_StoredProcedure from static readonly --> const

--- Libraries/Yutaka.Data/SqlUtil2.cs
+++ Libraries/Yutaka.Data/SqlUtil2.cs
@@ -11,11 +11,11 @@ namespace Yutaka.Data
 		/// <summary>
 		/// An SQL text command. (Default.)
 		/// </summary>
-		public static readonly CommandType CommandType_Text = CommandType.Text;
+		public const CommandType CommandType_Text = CommandType.Text;
 		/// <summary>
 		/// The name of a stored procedure.
 		/// </summary>
-		public static readonly CommandType CommandType_StoredProcedure = CommandType.StoredProcedure;
+		public const CommandType CommandType_StoredProcedure = CommandType.StoredProcedure;
 		public static readonly DateTime SqlDateTime_MinValue = new DateTime(1753, 1, 1);
 		public const CommandBehavior CommandBehavior_CloseConnection = CommandBehavior.CloseConnection;
 		#endregion Fields
