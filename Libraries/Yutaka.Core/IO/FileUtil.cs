c42f05d2be316546c9a9755bad58e708bf706a6d
Yutaka Blizman <blizman2017@gmail.com>
2022-06-07 02:06:01 -0700

    Deprecated TryDelete(). Use FastDelete(string) instead

--- Libraries/Yutaka.Core/IO/FileUtil.cs
+++ Libraries/Yutaka.Core/IO/FileUtil.cs
@@ -382,34 +382,18 @@ namespace Yutaka.Core.IO
 			return dt;
 		}
 
+		#region Deprecated
 		/// <summary>
-		/// Tries to deletes the specified file.
+		/// Deprecated June 7, 2022. Use FastDelete(string) instead.
 		/// </summary>
 		/// <param name="path">The name of the file to be deleted. Wildcard characters are not supported.</param>
 		/// <returns></returns>
+		[Obsolete("Deprecated June 7, 2022. Use FastDelete(string) instead.", false)]
 		public static int TryDelete(string path)
 		{
-			if (String.IsNullOrWhiteSpace(path)) {
-				Console.Write("{0}'path' is null or whitespace.{0}Exception thrown in FileUtil.TryDelete(string path).{0}", Environment.NewLine);
-				return 0;
-			}
-
-			try {
-				DeleteFile(path);
-				return 1;
-			}
-
-			catch (Exception ex) {
-				#region Log
-				if (ex.InnerException == null)
-					Console.Write("{0}{2}Exception thrown in FileUtil.TryDelete(string path='{3}').{2}{1}{2}{2}", ex.Message, ex.ToString(), Environment.NewLine, path);
-				else
-					Console.Write("{0}{2}Exception thrown in INNER EXCEPTION of FileUtil.TryDelete(string path='{3}').{2}{1}{2}{2}", ex.InnerException.Message, ex.InnerException.ToString(), Environment.NewLine, path);
-				#endregion Log
-
-				return 0;
-			}
+			return FastDelete(path);
 		}
+		#endregion
 		#endregion
 	}
 }
