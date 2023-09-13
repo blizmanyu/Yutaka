b7817d22a40b9a5790d26ce9ed3853344ff01f7b
Yutaka Blizman <blizman2017@gmail.com>
2023-09-13 01:57:58 -0700

    New Constructors FtpClient(string, int), FtpClient(string, NetworkCredential), FtpClient(string, int, NetworkCredential)

--- Libraries/Yutaka.Services/Ftp/FtpClient.cs
+++ Libraries/Yutaka.Services/Ftp/FtpClient.cs
@@ -46,8 +46,80 @@ namespace Yutaka.Services
 		/// <param name="port">An <see cref="int"/> greater than zero that contains the port to be used on &lt;host&gt;</param>
 		public FtpClient(string host, int port)
 		{
+			#region Check Input
+			var log = "";
+
+			if (String.IsNullOrWhiteSpace(host))
+				log = String.Format("{0}host is required.{1}", log, Environment.NewLine);
 			if (port < 1)
-				throw new ArgumentOutOfRangeException("port");
+				log = String.Format("{0}port must be greater than 0.{1}", log, Environment.NewLine);
+
+			if (!String.IsNullOrWhiteSpace(log))
+				throw new Exception(String.Format("{0}{1}Exception thrown in Constructor FtpClient(string host, int port).", log, Environment.NewLine));
+			#endregion
+
+			Host = host;
+			Port = port;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FtpClient"/> class using the specified FTP server and <see cref="NetworkCredential"/>s.
+		/// </summary>
+		/// <param name="host">A <see cref="string"/> that contains the name or IP address of the host computer used for FTP transactions.</param>
+		/// <param name="credentials">The <see cref="NetworkCredential"/>s for logging into the server.</param>
+		public FtpClient(string host, NetworkCredential credentials)
+		{
+			#region Check Input
+			var log = "";
+
+			if (String.IsNullOrWhiteSpace(host))
+				log = String.Format("{0}'host' is required.{1}", log, Environment.NewLine);
+			if (credentials == null)
+				log = String.Format("{0}'credentials' is required.{1}", log, Environment.NewLine);
+			else {
+				if (String.IsNullOrWhiteSpace(credentials.UserName))
+					log = String.Format("{0}'username' is required.{1}", log, Environment.NewLine);
+				if (String.IsNullOrWhiteSpace(credentials.Password))
+					log = String.Format("{0}'password' is required.{1}", log, Environment.NewLine);
+			}
+
+			if (!String.IsNullOrWhiteSpace(log))
+				throw new Exception(String.Format("{0}{1}Exception thrown in Constructor FtpClient(string host, NetworkCredential credentials).", log, Environment.NewLine));
+			#endregion
+
+			Host = host;
+			Port = 21;
+			Credentials = credentials;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FtpClient"/> class using the specified FTP server, port, and <see cref="NetworkCredential"/>s.
+		/// </summary>
+		/// <param name="host">A <see cref="string"/> that contains the name or IP address of the host computer used for FTP transactions.</param>
+		/// <param name="port">An <see cref="int"/> greater than zero that contains the port to be used on &lt;host&gt;</param>
+		/// <param name="credentials">The <see cref="NetworkCredential"/>s for logging into the server.</param>
+		/// <exception cref="Exception"></exception>
+		public FtpClient(string host, int port, NetworkCredential credentials)
+		{
+			#region Check Input
+			var log = "";
+
+			if (String.IsNullOrWhiteSpace(host))
+				log = String.Format("{0}'host' is required.{1}", log, Environment.NewLine);
+			if (port < 1)
+				log = String.Format("{0}port must be greater than 0.{1}", log, Environment.NewLine);
+			if (credentials == null)
+				log = String.Format("{0}'credentials' is required.{1}", log, Environment.NewLine);
+			else {
+				if (String.IsNullOrWhiteSpace(credentials.UserName))
+					log = String.Format("{0}'username' is required.{1}", log, Environment.NewLine);
+				if (String.IsNullOrWhiteSpace(credentials.Password))
+					log = String.Format("{0}'password' is required.{1}", log, Environment.NewLine);
+			}
+
+			if (!String.IsNullOrWhiteSpace(log))
+				throw new Exception(String.Format("{0}{1}Exception thrown in Constructor FtpClient(string host, int port, NetworkCredential credentials).", log, Environment.NewLine));
+			#endregion
 
 			Host = host;
 			Port = port;
